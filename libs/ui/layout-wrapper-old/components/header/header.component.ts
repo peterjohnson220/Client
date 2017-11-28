eb8845a9c64d4efecd3d3c156ef6e9d20dfe7441
Bennett Ballowe <Bennett.Ballowe@payfactors.com>
2017-11-28 15:35:56 -0500

    Initial commit. Creating  a new layout-wrapper folder and renaming the old layout-wrapper folder. Making the admin app reference the old layout-wrapper and the dashboard reference the new layout-wrapper.

---
+++ libs/ui/layout-wrapper-old/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+import { NavigationLink, UserContext } from '../../../../models';
+
+@Component({
+  selector: 'pf-layout-wrapper-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  @Input() avatarSource: string;
+  @Input() companyLogoSource: string;
+  @Input() gettingDropDownNavigationLinks: boolean;
+  @Input() gettingDropdownNavigationLinksError: boolean;
+
+  @Input() dropdownNavigationLinks: NavigationLink[];
+  @Input() userContext: UserContext;
+
+  constructor() {}
+}
---
+++ libs/ui/layout-wrapper-old/layout-wrapper-old.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { PfCommonUIModule } from '../common/common-ui-module';
+
+import { HeaderComponent, SidebarComponent } from './components';
+import { LayoutWrapperComponent } from './containers';
+import { HeaderEffects } from './effects';
+import { reducers } from './reducers';
+
+@NgModule({
+  imports:      [
+    CommonModule,
+    NgbModule,
+
+    PfCommonUIModule,
+
+    StoreModule.forFeature('layoutWrapper', reducers),
+    EffectsModule.forFeature([HeaderEffects]),
+  ],
+  declarations: [ HeaderComponent, SidebarComponent, LayoutWrapperComponent ],
+  exports:      [ HeaderComponent, SidebarComponent, LayoutWrapperComponent ]
+})
+export class PfLayoutWrapperOldModule { }
