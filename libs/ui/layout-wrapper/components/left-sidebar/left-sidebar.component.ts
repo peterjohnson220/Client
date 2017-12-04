f252d5dddf22b683717c4f986c9229f202355a4e
Bennett Ballowe <Bennett.Ballowe@payfactors.com>
2017-12-04 12:29:47 -0500

    Left and right sidebar changes

--- libs/ui/layout-wrapper/components/left-sidebar/left-sidebar.component.ts
+++ libs/ui/layout-wrapper/components/left-sidebar/left-sidebar.component.ts
@@ -7,24 +7,22 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LeftSidebarComponent implements OnInit {
-  @Input() leftSidebarToggle: boolean;
-
   sidebarLinks: any = [
-    { Name: 'Employees', Url: 'test url', NgAppLink: false, IconClass: 'fa-users' },
-    { Name: 'Structures', Url: 'test url', NgAppLink: false, IconClass: 'fa-bar-chart' },
-    { Name: 'Pricing Projects', Url: 'test url', NgAppLink: false, IconClass: 'fa-calculator' },
-    { Name: 'Data Insights', Url: 'test url', NgAppLink: false, IconClass: 'fa-pie-chart' },
-    { Name: 'Jobs', Url: 'test url', NgAppLink: false, IconClass: 'fa-sitemap' },
-    { Name: 'Surveys', Url: 'test url', NgAppLink: false, IconClass: 'fa-list-ul' },
-    { Name: 'Pay Markets', Url: 'test url', NgAppLink: false, IconClass: 'fa-area-chart' },
-    { Name: 'Job Descriptions', Url: 'test url', NgAppLink: false, IconClass: 'fa-file-text-o' },
-    { Name: 'Resources', Url: 'test url', NgAppLink: false, IconClass: 'fa-newspaper-o' },
-    { Name: 'Service', Url: 'test url', NgAppLink: false, IconClass: 'fa-question-circle-o' },
-    { Name: 'Community', Url: 'test url', NgAppLink: false, IconClass: 'fa-commenting-o' },
-    { Name: 'Activity', Url: 'test url', NgAppLink: false, IconClass: 'fa-bell-o' },
-    { Name: 'Data Diagnostics', Url: 'test url', NgAppLink: false, IconClass: 'fa-ambulance' },
-    { Name: 'Referrals', Url: 'test url', NgAppLink: false, IconClass: 'fa-thumbs-o-up' },
-    { Name: 'Ideas', Url: 'test url', NgAppLink: false, IconClass: 'fa-lightbulb-o' }
+    { Name: 'Employees', Url: 'test url', NgAppLink: true, IconClass: 'fa-users' },
+    { Name: 'Structures', Url: 'test url', NgAppLink: true, IconClass: 'fa-bar-chart' },
+    { Name: 'Pricing Projects', Url: 'test url', NgAppLink: true, IconClass: 'fa-calculator' },
+    { Name: 'Data Insights', Url: 'test url', NgAppLink: true, IconClass: 'fa-pie-chart' },
+    { Name: 'Jobs', Url: 'test url', NgAppLink: true, IconClass: 'fa-sitemap' },
+    { Name: 'Surveys', Url: 'test url', NgAppLink: true, IconClass: 'fa-list-ul' },
+    { Name: 'Pay Markets', Url: 'test url', NgAppLink: true, IconClass: 'fa-area-chart' },
+    { Name: 'Job Descriptions', Url: 'test url', NgAppLink: true, IconClass: 'fa-file-text-o' },
+    { Name: 'Resources', Url: 'test url', NgAppLink: true, IconClass: 'fa-newspaper-o' },
+    { Name: 'Service', Url: 'test url', NgAppLink: true, IconClass: 'fa-question-circle-o' },
+    { Name: 'Community', Url: 'test url', NgAppLink: true, IconClass: 'fa-commenting-o' },
+    { Name: 'Activity', Url: 'test url', NgAppLink: true, IconClass: 'fa-bell-o' },
+    { Name: 'Data Diagnostics', Url: 'test url', NgAppLink: true, IconClass: 'fa-ambulance' },
+    { Name: 'Referrals', Url: 'test url', NgAppLink: true, IconClass: 'fa-thumbs-o-up' },
+    { Name: 'Ideas', Url: 'test url', NgAppLink: true, IconClass: 'fa-lightbulb-o' }
   ];
   sidebarToggle = true;
   constructor() { }
