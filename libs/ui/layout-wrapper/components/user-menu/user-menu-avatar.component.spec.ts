66b08e512e76394d63c5689edabb06ccf750d579
DESKTOP-MME6UA5\mary.sylvia <mary.sylvia@payfactors.com>
2017-12-06 14:36:30 -0500

    Add unit tests

---
+++ libs/ui/layout-wrapper/components/user-menu/user-menu-avatar.component.spec.ts
@@ -0,0 +1,55 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+import { generateMockUserContext } from 'libs/models';
+
+import { UserMenuAvatarComponent } from './user-menu-avatar.component';
+
+describe('User Menu Avatar', () => {
+  let fixture: ComponentFixture<UserMenuAvatarComponent>;
+  let instance: UserMenuAvatarComponent;
+
+  // Configure Testing Module for before each test
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        UserMenuAvatarComponent
+      ],
+      // Shallow Testing
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
+
+    fixture = TestBed.createComponent(UserMenuAvatarComponent);
+    instance = fixture.componentInstance;
+  });
+  it('should show the user avatar when one exists', () => {
+
+    instance.userContext = generateMockUserContext();
+    instance.avatarSource = 'source';
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should show the users initials when user picture is set to default avatar', () => {
+
+    instance.userContext = generateMockUserContext();
+    instance.userContext.UserPicture = 'default_user.png';
+    instance.avatarSource = 'avatarsource';
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should show the users initials when user picture is null', () => {
+
+    instance.userContext = generateMockUserContext();
+    instance.userContext.UserPicture = null;
+    instance.avatarSource = 'source';
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+});
+
