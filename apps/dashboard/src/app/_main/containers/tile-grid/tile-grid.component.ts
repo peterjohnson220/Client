c452d02744b2add7b7e45627cf83bb0ec4fed45e
PF-SDIFABIO\Stefano.Difabio <stefano.difabio@payfactors.com>
2017-12-20 15:43:37 -0500

    Added dragula to UI, no backend wiring yet.

--- apps/dashboard/src/app/_main/containers/tile-grid/tile-grid.component.ts
+++ apps/dashboard/src/app/_main/containers/tile-grid/tile-grid.component.ts
@@ -3,6 +3,8 @@ import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
+import { DragulaService } from 'ng2-dragula';
+
 import * as fromTileGridReducer from '../../reducers';
 import * as fromTileGridActions from '../../actions/tile-grid.actions';
 import { Tile } from '../../models';
@@ -18,14 +20,32 @@ export class TileGridComponent implements OnInit {
   loadingError$: Observable<boolean>;
   tiles$: Observable<Tile[]>;
 
-  constructor(private store: Store<fromTileGridReducer.State>) {
+  constructor(private store: Store<fromTileGridReducer.State>,
+              private dragulaService: DragulaService) {
+    // Register Services
+    dragulaService.dropModel.subscribe((value) => {
+      TileGridComponent.onDrop(value.slice(1));
+    });
+
     // Register state
     this.loading$ = this.store.select(fromTileGridReducer.getTileGridLoading);
     this.loadingError$ = this.store.select(fromTileGridReducer.getTileGridLoadingError);
     this.tiles$ = this.store.select(fromTileGridReducer.getTileGridTiles);
   }
 
+  // Helper
+  private static getTileIndex(tile: any) {
+    return [].slice.call(tile.parentElement.children).indexOf(tile);
+  }
+
   // Events
+  private static onDrop(args) {
+    // const [tile, grid] = args; (tile and grid example)
+    const [tile] = args;
+    const tileIndex = TileGridComponent.getTileIndex(tile);
+    console.log('emit event reorder for index ' + tileIndex);
+  }
+
   handleTilesGridReload() {
     this.store.dispatch(new fromTileGridActions.LoadingTiles());
   }
