3e038d6fec4c98d6f1b05b5342bfd35790b41324
PF-SDIFABIO\Stefano.Difabio <stefano.difabio@payfactors.com>
2017-12-20 13:50:41 -0500

    Refactor tilegrid to use observables and remove them from dashboard containter since tilegrid is a "smart" component.

--- apps/dashboard/src/app/_main/containers/tile-grid/tile-grid.component.ts
+++ apps/dashboard/src/app/_main/containers/tile-grid/tile-grid.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import * as fromTileGridReducer from '../../reducers';
+import * as fromTileGridActions from '../../actions/tile-grid.actions';
 import { Tile } from '../../models';
 
 @Component({
@@ -7,9 +13,25 @@ import { Tile } from '../../models';
   styleUrls: ['./tile-grid.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TileGridComponent {
-  @Input() loading: boolean;
-  @Input() loadingError: boolean;
-  @Input() tiles: Tile[];
-  @Output() reload = new EventEmitter();
+export class TileGridComponent implements OnInit {
+  loading$: Observable<boolean>;
+  loadingError$: Observable<boolean>;
+  tiles$: Observable<Tile[]>;
+
+  constructor(private store: Store<fromTileGridReducer.State>) {
+    // Register state
+    this.loading$ = this.store.select(fromTileGridReducer.getTileGridLoading);
+    this.loadingError$ = this.store.select(fromTileGridReducer.getTileGridLoadingError);
+    this.tiles$ = this.store.select(fromTileGridReducer.getTileGridTiles);
+  }
+
+  // Events
+  handleTilesGridReload() {
+    this.store.dispatch(new fromTileGridActions.LoadingTiles());
+  }
+
+  // Lifecycle
+  ngOnInit() {
+    this.store.dispatch(new fromTileGridActions.LoadingTiles());
+  }
 }
