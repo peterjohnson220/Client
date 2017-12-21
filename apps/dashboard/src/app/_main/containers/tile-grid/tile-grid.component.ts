3bd77ad10c824f987d54dbbf495822452ade73d9
PF-SDIFABIO\Stefano.Difabio <stefano.difabio@payfactors.com>
2017-12-21 17:16:47 -0500

    Tile reording now saves to the db via existing Dashboard.ReArrangeTile endpoint. Only thing that's missing is UTs.

--- apps/dashboard/src/app/_main/containers/tile-grid/tile-grid.component.ts
+++ apps/dashboard/src/app/_main/containers/tile-grid/tile-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, Input } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
@@ -7,7 +7,10 @@ import { DragulaService } from 'ng2-dragula';
 
 import * as fromTileGridReducer from '../../reducers';
 import * as fromTileGridActions from '../../actions/tile-grid.actions';
+
+import { UserContext } from 'libs/models';
 import { Tile } from '../../models';
+import { ReorderTileRequest } from 'libs/models/dashboard';
 
 @Component({
   selector: 'pf-tile-grid',
@@ -19,12 +22,20 @@ export class TileGridComponent implements OnInit {
   loading$: Observable<boolean>;
   loadingError$: Observable<boolean>;
   tiles$: Observable<Tile[]>;
+  tiles: Tile[];
+  movedTileId: number;
+
+  @Input() userContext: UserContext;
 
   constructor(private store: Store<fromTileGridReducer.State>,
               private dragulaService: DragulaService) {
-    // Register Services
+    // subscribe to dragulaService events
     dragulaService.dropModel.subscribe((value) => {
-      TileGridComponent.onDrop(value.slice(1));
+      this.onDropModel(value.slice(1));
+    });
+
+    this.dragulaService.over.subscribe((value) => {
+      this.onOver(value.slice(1));
     });
 
     // Register state
@@ -33,25 +44,43 @@ export class TileGridComponent implements OnInit {
     this.tiles$ = this.store.select(fromTileGridReducer.getTileGridTiles);
   }
 
-  // Helper
+  // Helpers
   private static getTileIndex(tile: any) {
     return [].slice.call(tile.parentElement.children).indexOf(tile);
   }
 
-  // Events
-  private static onDrop(args) {
-    // const [tile, grid] = args; (tile and grid example)
-    const [tile] = args;
-    const tileIndex = TileGridComponent.getTileIndex(tile);
-    console.log('emit event reorder for index ' + tileIndex);
-  }
-
-  handleTilesGridReload() {
-    this.store.dispatch(new fromTileGridActions.LoadingTiles());
+  setTiles() {
+    this.tiles$.subscribe(tilesArray => {
+      this.tiles = tilesArray;
+    });
   }
 
   // Lifecycle
   ngOnInit() {
+    this.store.dispatch(new fromTileGridActions.LoadingTiles());
+    this.setTiles();
+  }
+
+  // Events
+  private onDropModel(args) {
+    const [tile] = args;
+    const tileIndex = TileGridComponent.getTileIndex(tile);
+    const nextTileIndex = tileIndex < (this.tiles.length - 1) ? tileIndex + 1 : null;
+    const nextTileId = nextTileIndex !== null ? this.tiles[nextTileIndex].Id : null;
+    const reorderTileRequest: ReorderTileRequest = {
+      movedUserTileId: this.movedTileId,
+      newNextUserTileId: nextTileId
+    };
+    this.store.dispatch(new fromTileGridActions.ReorderTiles(reorderTileRequest));
+  }
+
+  private onOver(args) {
+    const [tile] = args;
+    const movedTileIndex = TileGridComponent.getTileIndex(tile);
+    this.movedTileId = this.tiles[movedTileIndex].Id;
+  }
+
+  handleTilesGridReload() {
     this.store.dispatch(new fromTileGridActions.LoadingTiles());
   }
 }
