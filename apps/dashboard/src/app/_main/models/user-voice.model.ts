debaf243c14a7eb1a3ae6a023b0f26a5c0dfe40b
PF-SDIFABIO\Stefano.Difabio <stefano.difabio@payfactors.com>
2017-12-28 09:33:47 -0500

    Fixing user-voice-indicator UT.

--- apps/dashboard/src/app/_main/models/user-voice.model.ts
+++ apps/dashboard/src/app/_main/models/user-voice.model.ts
@@ -3,3 +3,11 @@ export interface UserVoiceModel {
   userVoiceUrl: string;
   userVoiceForumId: number;
 }
+
+export function generateMockUserVoiceModel(): UserVoiceModel {
+  return {
+    userId: 1,
+    userVoiceUrl: 'http://payfactors.uservoice.com/forums/',
+    userVoiceForumId: 268517
+  };
+}
