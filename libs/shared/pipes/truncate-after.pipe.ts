f4120ab926cb2a55e6b9fb1d623894dd0a373481
Stefano DiFabio <stefano.difabio@payfactors.com>
2018-01-11 15:36:48 -0500

    Added "TruncateAfter" pipe to shared libs.

---
+++ libs/shared/pipes/truncate-after.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'TruncateAfter'
+})
+export class TruncateAfterPipe implements PipeTransform {
+  transform(value: string, threshold: number, excludeEllipses?: boolean): string {
+    if (value.trim().length <= threshold) {
+      return value;
+    }
+
+    let truncatedString = value.trim().substring(0, threshold - 1).trim();
+    if (excludeEllipses == null || excludeEllipses !== true) {
+      truncatedString = truncatedString + '...';
+    }
+    return truncatedString;
+  }
+}
