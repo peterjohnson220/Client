11ba714b92cc1897ddf6c791a26ff298937efdaa
Brandon Collins <brandon.collins@payfactors.com>
2018-01-11 09:12:44 -0500

    Adding James grid reducer stuff

---
+++ libs/models/common/grid-type.enum.ts
@@ -0,0 +1,5 @@
+export enum GridTypeEnum {
+  AvailableCompanies = 'AVAILABLE_COMPANIES',
+  AvailableJobs = 'AVAILABLE_JOBS',
+  ExchangeJobMapping = 'EXCHANGE_JOB_MAPPING'
+}
--- libs/models/common/index.ts
+++ libs/models/common/index.ts
@@ -1,3 +1,4 @@
+export * from './grid-type.enum';
 export * from './validation-result-item.model';
 export * from './validation-result-item-type.enum';
 export * from './list-area-column.model';
