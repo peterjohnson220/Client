d3eebc2cbf6a65ba2032e34e2fce9b78e3d5f4f4
Brandon Collins <brandon.collins@payfactors.com>
2018-01-11 09:56:44 -0500

    Moved shared logic out to feature specific service. Updated main module with all the changes needed.

--- apps/peer/src/app/_main/main.module.ts
+++ apps/peer/src/app/_main/main.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PfSharedModule } from 'libs/shared';
-
-import { ExchangeListPageComponent } from './containers';
-import { MainRoutingModule } from './main-routing.module';
-import { PfCommonUIModule } from 'libs/ui/common';
 import { EffectsModule } from '@ngrx/effects';
-import { ExchangeListEffects } from './effects';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { StoreModule } from '@ngrx/store';
 
+import { PfFormsModule } from 'libs/forms';
+import { PfCommonUIModule } from 'libs/ui/common';
+import { PfSharedPeerModule } from 'libs/features';
 
+import { ExchangeListPageComponent, ExchangeJobMappingPageComponent, ExchangeJobMappingGridComponent } from './containers';
+import { ExchangeListEffects, ExchangeJobMappingEffects } from './effects';
+import { reducers } from './reducers';
+import { ExchangeJobMappingService } from './services';
+import { MainRoutingModule } from './main-routing.module';
 
 @NgModule({
   imports: [
@@ -17,18 +21,27 @@ import { ExchangeListEffects } from './effects';
     CommonModule,
 
     // 3rd party
-    EffectsModule.forFeature([ExchangeListEffects]),
+    GridModule,
+    StoreModule.forFeature('peerMain', reducers),
+    EffectsModule.forFeature([ExchangeJobMappingEffects, ExchangeListEffects]),
 
     // Routing
     MainRoutingModule,
 
     // Payfactors
     PfCommonUIModule,
-    PfSharedModule
+    PfSharedPeerModule,
+    PfFormsModule
   ],
   declarations: [
+    // Containers
+    ExchangeJobMappingGridComponent,
+
     // Pages
-    ExchangeListPageComponent
+    ExchangeListPageComponent, ExchangeJobMappingPageComponent
+  ],
+  providers: [
+    ExchangeJobMappingService
   ]
 })
 export class MainModule { }
