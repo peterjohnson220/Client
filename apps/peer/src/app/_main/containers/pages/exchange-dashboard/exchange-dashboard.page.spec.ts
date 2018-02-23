7f97688ce9d8969b7648b7e33f37bbd1053058c3
carlnunespf <carl.nunes@payfactors.com>
2018-02-23 10:41:25 -0500

    Added unit tests for the new page and updated the failing tests.

---
+++ apps/peer/src/app/_main/containers/pages/exchange-dashboard/exchange-dashboard.page.spec.ts
@@ -0,0 +1,92 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { StoreModule, Store, combineReducers } from '@ngrx/store';
+import { of } from 'rxjs/observable/of';
+import spyOn = jest.spyOn;
+
+import * as fromRootState from 'libs/state/state';
+import * as fromPeerMainReducer from '../../../reducers';
+import {  generateMockExchange } from 'libs/models';
+
+import { ExchangeDashboardPageComponent } from '../exchange-dashboard/exchange-dashboard.page';
+import * as fromExchangeDashboardActions from '../../../actions/exchange-dashboard.actions';
+
+describe('Peer - Exchange Dashboard', () => {
+  let fixture: ComponentFixture<ExchangeDashboardPageComponent>;
+  let instance: ExchangeDashboardPageComponent;
+  let router: Router;
+  let store: Store<fromPeerMainReducer.State>;
+  let activatedRoute: ActivatedRoute;
+
+  // Configure Testing Module for before each test
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          ...fromRootState.reducers,
+          peerMain: combineReducers(fromPeerMainReducer.reducers)
+        }),
+      ],
+      providers: [
+        {
+          provide: Router,
+          useValue: { navigate: jest.fn() },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { params: { id : 1 } } },
+        },
+      ],
+      declarations: [
+        ExchangeDashboardPageComponent
+      ],
+      // Shallow Testing
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
+
+    store = TestBed.get(Store);
+    router = TestBed.get(Router);
+    activatedRoute = TestBed.get(ActivatedRoute)
+
+    fixture = TestBed.createComponent(ExchangeDashboardPageComponent);
+    instance = fixture.componentInstance;
+
+    spyOn(store, 'dispatch');
+  });
+
+  it('should show the dashboard with the exchange name and a container for the charts', () => {
+    instance.exchange$ = of(generateMockExchange());
+
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should navigate to the job mapping page when clicking the manage jobs button', () => {
+    spyOn(router, 'navigate');
+
+    instance.manageJobsClick();
+
+    expect(router.navigate).toHaveBeenCalledWith(['exchange/job-mapping', activatedRoute.snapshot.params.id]);
+  });
+
+  it('should dispatch a industries loadingChart action on init', () => {
+    const action = new fromExchangeDashboardActions.LoadingChart({
+      ExchangeId: activatedRoute.snapshot.params.id,
+      ChartType: 'Industry'
+    });
+
+    instance.ngOnInit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should return the category for the label name', () => {
+    const expected = {
+      category: 'Category Name'
+    };
+    expect(instance.labelContent(expected)).toEqual(expected.category);
+  });
+});
