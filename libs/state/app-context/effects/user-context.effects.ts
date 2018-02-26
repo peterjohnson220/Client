ee384dad49cf6b19e972269251fcfc9a9e411f06
Bennett Ballowe <Bennett.Ballowe@payfactors.com>
2018-02-26 13:11:48 -0500

    Adding Actions for navigating the user back to the login page on a 401 error

--- libs/state/app-context/effects/user-context.effects.ts
+++ libs/state/app-context/effects/user-context.effects.ts
@@ -9,6 +9,7 @@ import 'rxjs/add/operator/catch';
 
 import { CompanySecurityApiService } from '../../../data/payfactors-api/index';
 import * as userContextActions from '../actions/user-context.actions';
+import { Router } from '@angular/router';
 
 
 @Injectable()
@@ -21,11 +22,33 @@ export class UserContextEffects {
       this.companySecurityApiService
         .getIdentity()
         .map((identity: any) => new userContextActions.GetUserContextSuccess(identity))
-        .catch(() => of(new userContextActions.GetUserContextError()))
+        .catch(error => {
+          return of(new userContextActions.GetUserContextError(error));
+        })
     );
 
-  constructor(
-    private actions$: Actions,
-    private companySecurityApiService: CompanySecurityApiService
-  ) {}
+  @Effect()
+  getUserContextError$ = this.actions$
+    .ofType(userContextActions.GET_USER_CONTEXT_ERROR)
+    .map((action: userContextActions.GetUserContextError) => action.error)
+    .switchMap(error => {
+        if (error.status === 401) {
+          return of(new userContextActions.GetUserContext401Error());
+        }
+      }
+    );
+
+  @Effect()
+  getUserContext401Error$ = this.actions$
+    .ofType(userContextActions.GET_USER_CONTEXT_401_ERROR)
+    .switchMap(() => {
+        window.location.href = `/login.asp?${window.location.pathname}`;
+        return null;
+      }
+    );
+
+  constructor(private actions$: Actions,
+              private companySecurityApiService: CompanySecurityApiService,
+              private router: Router) {
+  }
 }
