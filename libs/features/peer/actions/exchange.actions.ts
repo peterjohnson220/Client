5db7d77cd01d64ff4373734da843edd00331cce7
carlnunespf <carl.nunes@payfactors.com>
2018-02-26 10:14:43 -0500

    Updated the action namespace based on code review comment.

--- libs/features/peer/actions/exchange.actions.ts
+++ libs/features/peer/actions/exchange.actions.ts
@@ -2,9 +2,9 @@ import { Action } from '@ngrx/store';
 
 import { Exchange } from 'libs/models';
 
-export const LOADING_EXCHANGE  = '[Peer Admin/Exchange] Loading Exchange';
-export const LOADING_EXCHANGE_SUCCESS  = '[Peer Admin/Exchange] Loading Exchange Success';
-export const LOADING_EXCHANGE_ERROR  = '[Peer Admin/Exchange] Loading Exchange Error';
+export const LOADING_EXCHANGE  = '[Peer Shared] Loading Exchange';
+export const LOADING_EXCHANGE_SUCCESS  = '[Peer Shared] Loading Exchange Success';
+export const LOADING_EXCHANGE_ERROR  = '[Peer Shared] Loading Exchange Error';
 
 export class LoadingExchange implements Action {
   readonly type = LOADING_EXCHANGE;
