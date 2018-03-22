fe178ef78ea1d9fc23e384a756bab62c43a7f599
DESKTOP-MME6UA5\mary.sylvia <mary.sylvia@payfactors.com>
2018-03-21 20:59:17 -0400

    - lint spacing fix

--- libs/core/functions/user-voice-url.ts
+++ libs/core/functions/user-voice-url.ts
@@ -1,5 +1,5 @@
 import { environment } from 'environments/environment';
 
 export function userVoiceUrl(parentLink: string, userId: number): string {
-  return parentLink + '?userId='+ userId + '&url=' + environment.userVoiceUrl + environment.userVoiceForumId;
+  return parentLink + '?userId=' + userId + '&url=' + environment.userVoiceUrl + environment.userVoiceForumId;
 }
