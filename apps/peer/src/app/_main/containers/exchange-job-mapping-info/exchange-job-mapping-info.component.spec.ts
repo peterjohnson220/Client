fea03126576d98558a1fcebb7a6ef9293fae75f2
Brandon Collins <brandon.collins@payfactors.com>
2018-03-23 14:37:35 -0400

    Not sure why this is broken now, but we shouldn't need to callThrough.. Per the JEST documentation the spyON also calls the spied method

--- apps/peer/src/app/_main/containers/exchange-job-mapping-info/exchange-job-mapping-info.component.spec.ts
+++ apps/peer/src/app/_main/containers/exchange-job-mapping-info/exchange-job-mapping-info.component.spec.ts
@@ -3,6 +3,7 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { Store, combineReducers, StoreModule } from '@ngrx/store';
+import spyOn = jest.spyOn;
 
 import * as fromRootState from 'libs/state/state';
 import { generateMockExchangeJobMapping, UpsertExchangeJobMapRequest } from 'libs/models/peer';
@@ -11,7 +12,7 @@ import * as fromExchangeJobMappingInfoActions from '../../actions/exchange-job-m
 import * as fromExchangeJobMappingGridActions from '../../actions/exchange-job-mapping-grid.actions';
 import * as fromPeerMainReducer from '../../reducers';
 import { ExchangeJobMappingInfoComponent } from './exchange-job-mapping-info.component';
-import spyOn = jest.spyOn;
+
 
 describe('Peer - Exchange Job Mapping Info', () => {
   let fixture: ComponentFixture<ExchangeJobMappingInfoComponent>;
@@ -79,7 +80,7 @@ describe('Peer - Exchange Job Mapping Info', () => {
 
   it('should dispatch a LoadCompanyJobsToMapToByQuery action when a exchange job mapping that is not mapped is selected', () => {
     // Still need the actual implementation since we are making a setup call
-    spyOn(store, 'dispatch').and.callThrough();
+    spyOn(store, 'dispatch');
 
     instance.exchangeId = 1;
     fixture.detectChanges();
