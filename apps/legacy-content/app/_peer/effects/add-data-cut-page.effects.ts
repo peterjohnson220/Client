1ed21740814fc4284508bbbf83d17c43b6603d43
CORP\Bichngoc.McCulley <bichngoc.mcculley@payfactors.com>
2018-06-11 14:51:30 -0400

    refactor: update legacy-content app with rxjs v6 syntax

--- apps/legacy-content/app/_peer/effects/add-data-cut-page.effects.ts
+++ apps/legacy-content/app/_peer/effects/add-data-cut-page.effects.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import { Action, Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { Effect, Actions } from '@ngrx/effects';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/switchMap';
+import { of } from 'rxjs';
+import { map, catchError, switchMap, withLatestFrom, tap } from 'rxjs/operators';
 
 import { ExchangeCompanyApiService, ExchangeDataSearchApiService } from 'libs/data/payfactors-api/peer';
 import { WindowCommunicationService } from 'libs/core/services';
@@ -20,45 +17,51 @@ export class AddDataCutPageEffects {
 
   @Effect()
   addingDataCut$ = this.actions$
-    .ofType(fromAddDataCutPageActions.ADDING_DATA_CUT)
-    .map((action: fromAddDataCutPageActions.AddingDataCut) => action.payload)
-    .withLatestFrom(
-      this.peerMapStore.select(fromPeerMapReducers.getExchangeDataCutRequestData),
-      (action, exchangeDataCutRequestData) => ({action, exchangeDataCutRequestData}))
-    .switchMap((latest) => {
-      return this.exchangeCompanyApiService.addDataCut({
-        CompanyJobId: latest.action.CompanyJobId,
-        CompanyPayMarketId: latest.action.CompanyPayMarketId,
-        UserSessionId: latest.action.UserSessionId,
-        Filter: latest.exchangeDataCutRequestData,
-        ZoomLevel: latest.action.ZoomLevel,
-        PayMarketName: latest.exchangeDataCutRequestData.PayMarketName
+    .ofType(fromAddDataCutPageActions.ADDING_DATA_CUT).pipe(
+      map((action: fromAddDataCutPageActions.AddingDataCut) => action.payload),
+      withLatestFrom(
+        this.peerMapStore.select(fromPeerMapReducers.getExchangeDataCutRequestData),
+        (action, exchangeDataCutRequestData) => ({action, exchangeDataCutRequestData})
+      ),
+      switchMap((latest) => {
+        return this.exchangeCompanyApiService.addDataCut({
+          CompanyJobId: latest.action.CompanyJobId,
+          CompanyPayMarketId: latest.action.CompanyPayMarketId,
+          UserSessionId: latest.action.UserSessionId,
+          Filter: latest.exchangeDataCutRequestData,
+          ZoomLevel: latest.action.ZoomLevel,
+          PayMarketName: latest.exchangeDataCutRequestData.PayMarketName
+        }).pipe(
+          map(() => new fromAddDataCutPageActions.AddingDataCutSuccess()),
+          catchError(() => of(new fromAddDataCutPageActions.AddingDataCutError()))
+        );
       })
-      .map(() => new fromAddDataCutPageActions.AddingDataCutSuccess())
-      .catch(() => of(new fromAddDataCutPageActions.AddingDataCutError()));
-    });
+    );
 
   // Window Communication
   @Effect({ dispatch: false })
   addingDataCutSuccess$ = this.actions$
-    .ofType(fromAddDataCutPageActions.ADDING_DATA_CUT_SUCCESS)
-    .do((action: fromAddDataCutPageActions.AddingDataCutSuccess) => {
-      this.windowCommunicationService.postMessage(action.type);
-    });
+    .ofType(fromAddDataCutPageActions.ADDING_DATA_CUT_SUCCESS).pipe(
+      tap((action: fromAddDataCutPageActions.AddingDataCutSuccess) => {
+        this.windowCommunicationService.postMessage(action.type);
+      })
+  );
 
   @Effect({ dispatch: false })
   cancelAddDataCut$ = this.actions$
-    .ofType(fromAddDataCutPageActions.CANCEL_ADD_DATA_CUT)
-    .do((action: fromAddDataCutPageActions.CancelAddDataCut) => {
-      this.windowCommunicationService.postMessage(action.type);
-    });
+    .ofType(fromAddDataCutPageActions.CANCEL_ADD_DATA_CUT).pipe(
+      tap((action: fromAddDataCutPageActions.CancelAddDataCut) => {
+        this.windowCommunicationService.postMessage(action.type);
+      })
+  );
 
   @Effect({ dispatch: false })
   mapLoaded$ = this.actions$
-    .ofType(fromPeerMapActions.MAP_LOADED)
-    .do((action: fromPeerMapActions.MapLoaded) => {
-      this.windowCommunicationService.postMessage(action.type);
-    });
+    .ofType(fromPeerMapActions.MAP_LOADED).pipe(
+      tap((action: fromPeerMapActions.MapLoaded) => {
+        this.windowCommunicationService.postMessage(action.type);
+      })
+  );
 
   constructor(
     private actions$: Actions,
