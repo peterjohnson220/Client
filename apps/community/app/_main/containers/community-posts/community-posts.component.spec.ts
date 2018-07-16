d409411c7649ca41e38f264fd16c3f740f652214
Bennett Ballowe <Bennett.Ballowe@payfactors.com>
2018-07-16 12:22:40 -0400

    Initial Commit for BT-58

---
+++ apps/community/app/_main/containers/community-posts/community-posts.component.spec.ts
@@ -0,0 +1,51 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { generateMockCommunityPost } from '../../models/community-post';
+import { CommunityPostsComponent } from './community-posts.component';
+
+describe('CommunityPostsComponent', () => {
+  let component: CommunityPostsComponent;
+  let fixture: ComponentFixture<CommunityPostsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CommunityPostsComponent ],
+      // Shallow Testing
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommunityPostsComponent);
+    component = fixture.componentInstance;
+    component.LoadMockPosts = false;
+    fixture.detectChanges();
+  });
+
+  it('should show posts', () => {
+
+    component.CommunityPosts.push(generateMockCommunityPost(0, 'post text'));
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should change like button when there are likes', () => {
+
+    component.CommunityPosts.push(generateMockCommunityPost(55, 'post text'));
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should show tags', () => {
+    component.CommunityPosts.push(generateMockCommunityPost(55, 'post text',
+      [{ TagId: 'aa', TagName: 'Community tag'}, { TagId: 'aa', TagName: 'another tag'}]));
+
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+});
