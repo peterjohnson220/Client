c8769ecbb6d2b04908105af3cb132ab853eebb4a
CORP\dmitri.shvartsman <dmitri.shvartsman@payfactors.com>
2018-07-30 15:57:43 -0400

    BT-5: Use number for Status instead of enum. TODO: Introduce Status enum on .NET side.

--- libs/models/community/community-poll.model.ts
+++ libs/models/community/community-poll.model.ts
@@ -1,10 +1,8 @@
-import { CommunityPollStatusEnum } from '.';
-
 export interface CommunityPoll {
     CommunityPollId: number;
     Question: string;
     DatePosted: Date;
     CreatedByUser: number;
-    Status: CommunityPollStatusEnum;
+    Status: number;
     NumberOfResponses: number;
   }
