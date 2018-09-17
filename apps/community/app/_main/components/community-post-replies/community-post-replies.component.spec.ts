b0ac22bf3e4d0831312beef46787df59eff78d66
CORP\dmitri.shvartsman <dmitri.shvartsman@payfactors.com>
2018-09-17 14:10:46 -0400

    BT-103: Adding likes to community post replies (Client).

--- apps/community/app/_main/components/community-post-replies/community-post-replies.component.spec.ts
+++ apps/community/app/_main/components/community-post-replies/community-post-replies.component.spec.ts
@@ -6,13 +6,13 @@ import { combineReducers, Store, StoreModule } from '@ngrx/store';
 
 import * as fromRootState from 'libs/state/state';
 import * as fromCommunityPostReducer from '../../reducers';
+import * as fromCommunityPostActions from '../../actions/community-post.actions';
 
 import { CommunityPostRepliesComponent } from './community-post-replies.component';
 
-
 describe('CommunityPostsComponent', () => {
-  let fixture: ComponentFixture<CommunityPostsRepliesComponent>;
-  let instance: CommunityPostsRepliesComponent;
+  let fixture: ComponentFixture<CommunityPostRepliesComponent>;
+  let instance: CommunityPostRepliesComponent;
   let store: Store<fromRootState.State>;
 
   // Configure Testing Module for before each test
@@ -21,7 +21,7 @@ describe('CommunityPostsComponent', () => {
       imports: [
         StoreModule.forRoot({
           ...fromRootState.reducers,
-          communityPollRequest: combineReducers(fromCommunityPostReducer.reducers)
+          communityPost: combineReducers(fromCommunityPostReducer.reducers)
         }),
         ReactiveFormsModule
       ],
@@ -54,5 +54,16 @@ describe('CommunityPostsComponent', () => {
     fixture.detectChanges();
 
     expect(fixture).toMatchSnapshot();
+  });
+
+  it('should dispatch UpdatingCommunityPostReplyLike when calling updateReplyLike', () => {
+    const likedByCurrentUser = true;
+    const replyPayload = {PostId: '1234', Id: '12345', LikedByCurrentUser: likedByCurrentUser};
+    instance.updateReplyLike(replyPayload);
+
+    const replyResult = {postId: '1234', replyId: '12345', like: !likedByCurrentUser};
+    const expectedAction = new fromCommunityPostActions.UpdatingCommunityPostReplyLike(replyResult);
+
+    expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
   });
 });
