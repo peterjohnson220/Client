8941c116be1e6157786eedbb0a2b3744cb752c27
Mattias Andries <mattias.andries@payfactors.com>
2018-11-02 15:34:13 -0400

    CORE-167 and CORE-168

--- apps/admin/app/_org-data-loader/org-data-loader-routing.module.ts
+++ apps/admin/app/_org-data-loader/org-data-loader-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PfAdminGuard } from 'libs/security/guards';
 import { ManageFieldMappingsPageComponent } from './containers/pages';
 
 const routes: Routes = [
-  { path: 'manage-field-mappings', component: ManageFieldMappingsPageComponent},
+  { path: 'manage-field-mappings', component: ManageFieldMappingsPageComponent, canActivate: [PfAdminGuard]},
   { path: '', redirectTo: 'manage-field-mappings', pathMatch: 'full' },
 ];
 
