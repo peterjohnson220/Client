f67a542d884b5b263abc4b8854706f2088f26e4c
Adam Colson <Adam.Colson@payfactors.com>
2018-11-05 13:21:54 -0500

    add Request Access button to login page

--- environments/environment.staging.ts
+++ environments/environment.staging.ts
@@ -14,6 +14,7 @@ export const environment = {
   tableauPublicReportingServer: 'https://public.tableau.com/',
   defaultHomePage: '/client/dashboard',
   firstTimeLoginPage: '/client/auth/first-login',
+  allowSelfRegistration: true,
   smallBusinessApiUrl: 'https://smallbiz.payfactors.com/api/',
   smallBusinessAppUrl: 'https://smallbiz.payfactors.com/app/'
 };
