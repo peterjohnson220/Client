79bbfe04f8a018045c07f17137115a567cfc6ec6
CORP\dmitri.shvartsman <dmitri.shvartsman@payfactors.com>
2018-12-06 15:52:44 -0500

    BT-238: Community tile preview for polls (Client).

--- apps/dashboard/app/_main/components/tile-preview/chart-with-list/tile-preview-chart-with-list.component.ts
+++ apps/dashboard/app/_main/components/tile-preview/chart-with-list/tile-preview-chart-with-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import * as cloneDeep from 'lodash.clonedeep';
+
+import { Component, Input, OnInit } from '@angular/core';
 
 import { TilePreviewCharWithList } from '../../../models';
 
@@ -7,9 +9,31 @@ import { TilePreviewCharWithList } from '../../../models';
   templateUrl: './tile-preview-chart-with-list.component.html',
   styleUrls: [ './tile-preview-chart-with-list.component.scss' ]
 })
-export class TilePreviewChartWithListComponent {
+export class TilePreviewChartWithListComponent  implements OnInit {
 
   @Input() model: TilePreviewCharWithList;
+  chartData: any;
+  MIN_CHART_DISPLAY_VALUE = 2.0001;
+
+  ngOnInit() {
+    this.chartData = cloneDeep(this.model.TileChartData);
+
+    if ( this.hasValidResponses() ) { this.chartData.forEach(x => {
+        if (x.Percentage === 0) { x.Percentage = this.MIN_CHART_DISPLAY_VALUE; }}); // to render in the chart
+    }
+  }
+
+  navigateToPost(): void {
+    window.location.href  = this.model.PostUrl;
+  }
+
+  hasValidResponses(): boolean {
+    return this.model.TileChartData ? this.model.TileChartData.find(x => x.Percentage > 0) : false;
+  }
+
+  getPercentage(value): number {
+    return value === this.MIN_CHART_DISPLAY_VALUE ? 0 : value;
+  }
 
 }
 
