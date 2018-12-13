d542c856b47afcc8f09a1ac72f8e7d4b6cf4c3f2
Brandon Collins <brandon.collins@payfactors.com>
2018-12-13 11:03:44 -0500

    TB-2458: Exclude Production Data from Pre-Pended Pendo IDs

--- assets/js/pendo.js
+++ assets/js/pendo.js
@@ -14,17 +14,26 @@ function initializePendo(userContext)
     var account = {};
 
     if (userContext){
+      // This is under the assumption this file is being served from the application server and
+      // that our production subdomain is pf. Excluding production url from prepending subdomain to retain
+      // pendo history.
       var subDomain = window.location.host.split('.')[0];
-      var visitor = {
-        id: subDomain + "_" + userContext.UserId, // Required if user is logged in
+      var idPrefix = '';
+
+      if(subDomain !== 'pf') {
+        idPrefix = subDomain + "_";
+      }
+
+      visitor = {
+        id: idPrefix + userContext.UserId, // Required if user is logged in
         email: userContext.EmailAddress // Optional
         // role:         // Optional
 
         // You can add any additional visitor level key-values here,
         // as long as it's not one of the above reserved names.
       };
-      var account = {
-        id: subDomain + "_" + userContext.CompanyId, // Highly recommended
+      account = {
+        id: idPrefix + userContext.CompanyId, // Highly recommended
         name: userContext.CompanyName // Optional
         // planLevel:    // Optional
         // planPrice:    // Optional
