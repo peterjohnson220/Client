e19f701b472fe56de3408b46feb36d1d79fefe8f
Brandon Collins <brandon.collins@payfactors.com>
2018-12-20 16:08:13 -0500

    TB-2479: Add EEO and FLSA Status to Jobs Index

--- apps/project/app/_add-jobs/helpers/payfactors-add-jobs-api-model-mapper.helper.ts
+++ apps/project/app/_add-jobs/helpers/payfactors-add-jobs-api-model-mapper.helper.ts
@@ -24,6 +24,7 @@ export class PayfactorsAddJobsApiModelMapper {
         FLSAStatus: jsr.FLSAStatus,
         Category: jsr.Category,
         Level: jsr.Level,
+        EEO: jsr.EEO,
         UdfFields: jsr.UdfFields,
         IsSelected: false,
         IsPayfactorsJob: jsr.IsPayfactorsJob
