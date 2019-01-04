ec164dcd32679ea08d94459ef5f1f8146c1b00e1
BrendanGrimesPF <brendan.grimes@payfactors.com>
2019-01-04 11:31:37 -0500

    TB-2428 - View Exchange Scope Description

--- libs/models/peer/exchange-scope/exchange-scope-item.model.ts
+++ libs/models/peer/exchange-scope/exchange-scope-item.model.ts
@@ -1,11 +1,13 @@
 export interface ExchangeScopeItem {
   Id: string;
   Name: string;
+  Description: string;
 }
 
 export function generateMockExchangeScopeItem(): ExchangeScopeItem {
   return {
     Id: 'MockExchangeScopeItemId',
-    Name: 'MockExchangeScopeItemName'
+    Name: 'MockExchangeScopeItemName',
+    Description: 'MockExchangeScopeItemDescription'
   };
 }
