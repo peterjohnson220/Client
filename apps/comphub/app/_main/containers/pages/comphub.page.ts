16135829b1c0fb2448c575a272563d7fc830fac4
Brandon Collins <brandon.collins@payfactors.com>
2019-01-07 13:22:38 -0500

    TB-2516: Standup New Comphub App

---
+++ apps/comphub/app/_main/containers/pages/comphub.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pf-comphub-page',
+  templateUrl: './comphub.page.html',
+  styleUrls: ['./comphub.page.scss']
+})
+export class ComphubPageComponent {
+
+  constructor() {}
+}
