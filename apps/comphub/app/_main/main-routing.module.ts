8b658da4a8d84924e9db6da86366d3795809eda0
CORP\Bichngoc.McCulley <bichngoc.mcculley@payfactors.com>
2019-01-08 16:51:37 -0500

    TB-2517: Comphub - Jobs Screen

--- apps/comphub/app/_main/main-routing.module.ts
+++ apps/comphub/app/_main/main-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ComphubPageComponent } from './containers/pages';
+import { ComphubPageComponent, JobsPageComponent } from './containers/pages';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ComphubPageComponent
-  }
+  { path: '', component: ComphubPageComponent },
+  { path: 'jobs', component: JobsPageComponent }
 ];
 
 @NgModule({
