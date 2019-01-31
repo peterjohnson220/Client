d1817a06c7a908de1b2979681a87ba6f281a0249
Rondel <rondel.ward@payfactors.com>
2019-01-31 12:15:25 -0500

    TB-2639 - added sorting to grid

--- libs/models/payfactors-api/comphub/request/quick-price-request.model.ts
+++ libs/models/payfactors-api/comphub/request/quick-price-request.model.ts
@@ -4,5 +4,10 @@ export interface QuickPriceRequest {
   JobTitleShort: string;
   CompanyPaymarketId?: number;
   PagingOptions: PagingOptions;
+  Sort?: SortOption;
 }
 
+export interface SortOption {
+  Field: string;
+  Dir: string;
+}
