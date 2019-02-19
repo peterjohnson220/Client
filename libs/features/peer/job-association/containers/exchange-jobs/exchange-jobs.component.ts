5c43870a3bdc02893f91bc41cb82e76665071fb3
Adam Colson <Adam.Colson@payfactors.com>
2019-02-19 11:20:47 -0500

    exchange search by job title in job assoc modal

--- libs/features/peer/job-association/containers/exchange-jobs/exchange-jobs.component.ts
+++ libs/features/peer/job-association/containers/exchange-jobs/exchange-jobs.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
 import { State } from '@progress/kendo-data-query';
 import { TooltipDirective } from '@progress/kendo-angular-tooltip';
 
+import { InputDebounceComponent } from 'libs/forms/components';
 import { GridTypeEnum } from 'libs/models/common';
 import * as fromPeerJobsReducer from '../../reducers';
 import * as peerExchangeJobsActions from '../../actions/exchange-jobs.actions';
@@ -16,14 +17,18 @@ import * as fromGridActions from 'libs/core/actions/grid.actions';
   templateUrl: './exchange-jobs.component.html',
   styleUrls: ['./exchange-jobs.component.scss']
 })
-export class ExchangeJobsComponent implements OnInit {
+export class ExchangeJobsComponent implements OnInit, OnDestroy {
   @ViewChild(TooltipDirective) public tooltipDir: TooltipDirective;
+  @ViewChild(InputDebounceComponent) public jobTitleSearchComponent: InputDebounceComponent;
 
   peerExchangeJobs$: Observable<GridDataResult>;
   totalPeerExchangeJobs$: Observable<number>;
   gridState$: Observable<State>;
   loading$: Observable<boolean>;
   loadingError$: Observable<boolean>;
+
+  searchTerm: string;
+  searchTermSubscription: Subscription;
 
   constructor(private store: Store<fromPeerJobsReducer.State>) { }
 
@@ -33,12 +38,20 @@ export class ExchangeJobsComponent implements OnInit {
     this.gridState$ = this.store.select(fromPeerJobsReducer.getExchangeJobsGridState);
     this.loading$ = this.store.select(fromPeerJobsReducer.getExchangeJobsLoading);
     this.loadingError$ = this.store.select(fromPeerJobsReducer.getExchangeJobsLoadingError);
+    this.searchTermSubscription = this.store.select(fromPeerJobsReducer.getExchangeJobsSearchTerm).subscribe(
+      (searchTerm) => this.searchTerm = searchTerm
+    );
 
     this.store.dispatch(new peerExchangeJobsActions.LoadExchangeJobs());
   }
 
-  reload(): void {
-    this.store.dispatch(new peerExchangeJobsActions.LoadExchangeJobs());
+  reload(resetSearchTerm = false): void {
+    // if this is invoked from an empty search results grid reset the term, otherwise keep the term as is and reload
+    if (resetSearchTerm) {
+      this.jobTitleSearchComponent.clearValue();
+    } else {
+      this.store.dispatch(new peerExchangeJobsActions.LoadExchangeJobs());
+    }
   }
 
   showTooltip(e: any): void {
@@ -52,5 +65,18 @@ export class ExchangeJobsComponent implements OnInit {
   handleDataStateChange(state: DataStateChangeEvent): void {
     this.store.dispatch(new fromGridActions.UpdateGrid(GridTypeEnum.JobAssociationModalPeerExchangeJobs, state));
     this.store.dispatch(new peerExchangeJobsActions.LoadExchangeJobs());
+  }
+
+  updateSearchFilter(searchTerm: string): void {
+    this.store.dispatch(new peerExchangeJobsActions.UpdateSearchTerm(searchTerm));
+
+    // only search if 2+ chars are supplied, or if the term is reset and it's empty
+    if (!searchTerm || searchTerm.length > 1) {
+      this.store.dispatch(new peerExchangeJobsActions.LoadExchangeJobs());
+    }
+  }
+
+  ngOnDestroy() {
+    this.searchTermSubscription.unsubscribe();
   }
 }
