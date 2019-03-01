96da2072b814ae3b3fb4be0cdada75c77e48942e
CORP\Bichngoc.McCulley <bichngoc.mcculley@payfactors.com>
2019-03-01 12:17:01 -0500

    TB-2694: CompHub Glossary of Terms for SMB

---
+++ apps/comphub/app/_main/components/glossary-of-terms/glossary-of-terms.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pf-glossary-of-terms',
+  templateUrl: './glossary-of-terms.component.html',
+  styleUrls: ['./glossary-of-terms.component.scss']
+})
+export class GlossaryOfTermsComponent {
+
+  isOpen = false;
+
+  constructor() { }
+
+}
--- apps/comphub/app/_main/components/index.ts
+++ apps/comphub/app/_main/components/index.ts
@@ -5,3 +5,4 @@ export * from './add-paymarket-form';
 export * from './salary-bar-chart';
 export * from './salary-trend-chart';
 export * from './share-pricing-summary-modal';
+export * from './glossary-of-terms';
