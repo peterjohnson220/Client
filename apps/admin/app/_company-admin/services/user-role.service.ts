4dca98e9faa1acce23dc01939abc7933c1404866
Erik B <erik.butcher@payfactors.com>
2019-04-10 10:27:22 -0400

    Core-688 - Add Edit Role Name and UI Updates
    Add Edit Role Name and UI Updates
    Building out layout, stripped down tab css
    
    Building out layout, stripped down tab css
    Simplified layouts and scrolling
    
    Simplified layouts and scrolling, introduced scrolling to tab card shell and removed internal scrolling
    Updates in Tabs
    
    Updates to clean up the tabs
    Cleaned up Padding and Margins
    
    Cleaned up padding and margins
    Updated for Firefox and IE Compatibility
    
    Updated for Firefox and IE Compatibility
    Almost Complete
    
    Need to resolve error with clickelsewhere otherwise looks ready for FR
    Fixed Clickelsewhere and Cleanup
    
    Fixed Clickelsewhere and Cleanup
    Visual Updates
    
    Visual Updates

--- apps/admin/app/_company-admin/services/user-role.service.ts
+++ apps/admin/app/_company-admin/services/user-role.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { UserAssignedRole } from 'libs/models/security';
+import { RolesApiService } from 'libs/data/payfactors-api/company-admin';
 
 import * as fromUserRoleViewReducer from '../reducers';
 import * as fromUserRoleViewActions from '../actions/user-role-view.action';
@@ -10,7 +11,7 @@ import {UserRoleTabState} from '../constants/user-role.constants';
 
 @Injectable()
 export class UserRoleService {
-  constructor(private store: Store<fromUserRoleViewReducer.State>) {
+  constructor(private store: Store<fromUserRoleViewReducer.State>,  private adminRolesApi: RolesApiService) {
   }
 
   updateCurrentUserRoleViewTabState(userRoleViewTabState: UserRoleTabState) {
@@ -19,5 +20,9 @@ export class UserRoleService {
 
   updateCurrentUserRole(userRole: UserAssignedRole) {
     this.store.dispatch(new fromUserRoleViewActions.UpdateCurrentUserRole(userRole));
+  }
+
+  updateCurrentRoleName(newRoleName: string, roleId: number) {
+    return this.adminRolesApi.updateRoleName(newRoleName, roleId);
   }
 }
