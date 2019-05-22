9a7d543cc31eb63cfac3957b2697f546cee1dde8
andrew-gallant-payfactors <andrew.gallant@payfactors.com>
2019-05-22 18:09:04 -0400

    CORE-646
    - added add/full replace radio buttons to field mappings
    - refactored loading/saving settings to support the new settings
    -- autoloaders enabled/disabled, and entity loads enabled/disabled are saved, but not toggleable in the UI yet

---
+++ apps/admin/app/_org-data-loader/models/loader-entity-status.model.ts
@@ -0,0 +1,8 @@
+export interface LoaderEntityStatus {
+  complete: boolean;
+  dateFormat?: string;
+  isFullReplace?: boolean;
+  loadEnabled?: boolean;
+  loaderType: string;
+  mappings?: string[];
+}
