71625ec1096d8d77d85abbbb577c1a909ee71d6d
hhuynhpf <hung.huynh@payfactors.com>
2019-06-19 13:56:58 -0400

    TB-3385v_2

--- apps/peer/app/_dashboard/fa-icons.ts
+++ apps/peer/app/_dashboard/fa-icons.ts
@@ -1,7 +1,7 @@
-import { faBuilding }  from "@fortawesome/pro-solid-svg-icons/faBuilding";
+import { faBuilding } from "@fortawesome/pro-solid-svg-icons/faBuilding";
 import { faBriefcase } from "@fortawesome/pro-solid-svg-icons/faBriefcase";
 import { faTimes } from "@fortawesome/pro-regular-svg-icons/faTimes";
-import { faInfoCircle } from "@fortawesome/pro-solid-svg-icons/faInfoCircle";
+import { faInfoCircle } from "@fortawesome/pro-regular-svg-icons/faInfoCircle";
 import { faUsdCircle } from "@fortawesome/pro-solid-svg-icons/faUsdCircle";
 import { faUsers } from "@fortawesome/pro-solid-svg-icons/faUsers";
 import { faCaretDown } from "@fortawesome/pro-solid-svg-icons/faCaretDown";
