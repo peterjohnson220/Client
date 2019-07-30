4b092330e9008dd678eb2c5d4f390426b13fad73
CORP\dmitri.shvartsman <dmitri.shvartsman@payfactors.com>
2019-07-30 13:11:45 -0400

    FORT-5: Topic required field.

--- libs/models/community/community-add-post.model.ts
+++ libs/models/community/community-add-post.model.ts
@@ -1,7 +1,9 @@
 import { CommunityLink } from './community-link.model';
+import { CommunityTopic } from './community-topic.model';
 
 export interface CommunityAddPost {
   PostText: string;
   IsInternalOnly: boolean;
   Links: CommunityLink[];
+  TopicId: string;
 }
