d0d3506f12fea969bdf88ee6dab2eb9ec6f600f4
gregleclair <greg.leclair@payfactors.com>
2019-08-19 09:42:59 -0400

    ENG-27

---
+++ apps/jobs/app/_jobs-page/fa-icons.ts
@@ -0,0 +1,8 @@
+import { faDownload } from '@fortawesome/pro-solid-svg-icons/faDownload';
+import { faTable } from '@fortawesome/pro-solid-svg-icons/faTable';
+import { faSlidersH } from '@fortawesome/pro-solid-svg-icons/faSlidersH';
+
+export const faIcons = [
+  faDownload, faTable, faSlidersH
+];
+
