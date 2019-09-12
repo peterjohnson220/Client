b71e362d31826fc6d43d221e7a63a29d495ad563
derekpayfactors <derek.gilbert@payfactors.com>
2019-09-12 10:10:15 -0400

    SUP17: Copy survey enhancements

--- apps/admin/app/_survey-library/survey-library.module.ts
+++ apps/admin/app/_survey-library/survey-library.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule, NgbPopoverModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { NgbModule, NgbPopoverModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { ComboBoxModule } from '@progress/kendo-angular-dropdowns';
+
+import { PfCommonModule } from 'libs/core';
+import { PfApiModule, SurveyLibraryApiService } from 'libs/data/payfactors-api';
 import { PfFormsModule } from 'libs/forms';
 import { PfCommonUIModule } from 'libs/ui/common';
-import { PfApiModule, SurveyLibraryApiService } from 'libs/data/payfactors-api';
-import { PfCommonModule } from 'libs/core';
 
 import * as fromFaIcons from './fa-icons';
 import { SurveyComponent } from './containers/survey/survey.component';
@@ -19,17 +22,14 @@ import { SurveyLibraryRoutingModule } from './survey-library-routing.module';
 import { AddSurveyModalComponent } from './containers/add-survey-modal/add-survey-modal.component';
 import { SurveyLibraryStateService } from './services/survey-library-state.service';
 import {
-  AddSurveyTitleModalComponent,
-  CustomSurveyTitleComponent,
-  SurveyTitleListItemComponent,
-  SurveyTitlesPageComponent,
-  MapCompanyModalComponent
+    AddSurveyTitleModalComponent, CustomSurveyTitleComponent, MapCompanyModalComponent, SurveyTitleListItemComponent,
+    SurveyTitlesPageComponent
 } from './containers';
-import { SurveyTitleCompaniesListComponent, ExpandCollapseIconComponent } from './components';
+import { ExpandCollapseIconComponent, SurveyTitleCompaniesListComponent } from './components';
 import { CollapserDirective } from './directives';
 import { reducers } from './reducers';
-import { CompanySelectorEffects, SurveyTitlesEffects, SurveyEffects } from './effects';
-import { ComboBoxModule } from '@progress/kendo-angular-dropdowns';
+import { CompanySelectorEffects, SurveyEffects, SurveyTitlesEffects } from './effects';
+import { CopySurveyModalComponent } from './containers/copy-survey-modal/copy-survey-modal.component';
 
 @NgModule({
   imports: [
@@ -73,6 +73,7 @@ import { ComboBoxModule } from '@progress/kendo-angular-dropdowns';
     CustomSurveyTitleComponent,
     SurveyTitleCompaniesListComponent,
     ExpandCollapseIconComponent,
+    CopySurveyModalComponent,
 
     // Directives
     CollapserDirective
