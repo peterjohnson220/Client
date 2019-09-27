ea958db90273ac1a7da953b2b72f80aeee3c93d5
CORP\Bichngoc.McCulley <bichngoc.mcculley@payfactors.com>
2019-09-27 12:13:04 -0400

    IDEA-183: Enforce date range to be at least one date apart

--- apps/data-insights/app/_main/components/date-range-filter/date-range-filter.component.ts
+++ apps/data-insights/app/_main/components/date-range-filter/date-range-filter.component.ts
@@ -2,6 +2,8 @@ import { Component, Output, Input, EventEmitter } from '@angular/core';
 
 import { SelectionRange } from '@progress/kendo-angular-dateinputs';
 import { IntlService } from '@progress/kendo-angular-intl';
+import { addDays, isEqualDate } from '@progress/kendo-date-math';
+
 import { FilterOperator, BetweenOperator } from '../../models';
 
 @Component({
@@ -29,6 +31,9 @@ export class DateRangeFilterComponent {
   handleSelectionRangeChange(range: SelectionRange): void {
     if (!range.start || !range.end) {
       return;
+    }
+    if (isEqualDate(range.start, range.end)) {
+      range.end = addDays(range.start, 1);
     }
     this.selectionRangeChanged.emit(range);
   }
