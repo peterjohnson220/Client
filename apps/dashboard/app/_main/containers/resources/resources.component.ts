f7786610f6bc880dbe174add2108b41a61d01c40
CORP\dmitri.shvartsman <dmitri.shvartsman@payfactors.com>
2019-09-30 11:37:45 -0400

    FORT-105: Resources page redirects to "resources.asp" until new company-resource" page is ready

--- apps/dashboard/app/_main/containers/resources/resources.component.ts
+++ apps/dashboard/app/_main/containers/resources/resources.component.ts
@@ -10,6 +10,13 @@ export class ResourcesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+    window.onmessage = function(e) {
+      if (e.data === 'MyResources') {
+        window.location.href  = '/marketdata/resources.asp';
+        // TODO: redirecting to asp resources until new company-resources page is ready
+        // window.location.href  = '/client/dashboard/company-resources';
+      }
+    };
+  }
 }
