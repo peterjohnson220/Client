0838c0168e881f01a022d502364bf2138ecafa4c
Brandon Collins <brandon.collins@payfactors.com>
2019-10-31 14:32:14 -0400

    PEER-191: Back Button Fixes

--- libs/ui/common/page-layout/page-with-title-bar/page-with-title-bar.component.ts
+++ libs/ui/common/page-layout/page-with-title-bar/page-with-title-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { RouteTrackingService } from 'libs/core/services';
 
@@ -11,16 +11,20 @@ import { RouteTrackingService } from 'libs/core/services';
 export class PageWithTitleBarComponent {
   @Input() isFullHeader = true;
   @Input() returnUrl: string;
+  @Input() relativeReturn: boolean;
   @Input() goBackToPreviousRoute: boolean;
   @Input() absoluteUrl: string;
   @Input() contentNoPadding: boolean;
 
   constructor(
     private routeTrackingService: RouteTrackingService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   goBack() {
-    this.goBackToPreviousRoute ? this.routeTrackingService.goBack() : this.router.navigate([this.returnUrl]);
+    this.goBackToPreviousRoute
+      ? this.routeTrackingService.goBack()
+      : this.router.navigate([this.returnUrl], this.relativeReturn ? { relativeTo: this.route } : {});
   }
 }
