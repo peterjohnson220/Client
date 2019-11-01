42fc2c435c21eac3a00c375a69fbaf914db36355
Daniel Cardenas <daniel.cardenas@payfactors.com>
2019-11-01 10:27:41 -0400

    Base Companies API

--- apps/admin/app/_survey-library/helpers/payfactors-api-model-mapper.ts
+++ apps/admin/app/_survey-library/helpers/payfactors-api-model-mapper.ts
@@ -1,13 +1,13 @@
-import { Company } from '../../../../../libs/models/company';
+import { CompanyBaseInformation } from 'libs/models/company';
 import { CompanySelectorItem } from '../models';
 
 export class PayfactorsApiModelMapper {
 
-  static mapCompaniesResponseToCompanySelector(companies: Company[]): CompanySelectorItem[] {
+  static mapCompaniesResponseToCompanySelector(companies: CompanyBaseInformation[]): CompanySelectorItem[] {
     return companies.map(c => {
       return {
         CompanyId: c.CompanyId,
-        CompanyName: c.CompanyName
+        CompanyName: c.Name
       };
     });
   }
