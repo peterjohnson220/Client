2f01394fe1b6d914b13309a6a0211416ca9358a3
CORP\James.Prehemo <james.prehemo@payfactors.com>
2019-11-14 11:59:20 -0500

    PEER-214 - [PEER MAPS] 500 Console Error Thrown when Filters or an Exchange Scope (with filter selections) are Applied

--- libs/features/peer/exchange-explorer/actions/exchange-filter-context.actions.ts
+++ libs/features/peer/exchange-explorer/actions/exchange-filter-context.actions.ts
@@ -5,7 +5,6 @@ import { ExchangeScopeItem, ExchangeDataSearchFilterContext } from 'libs/models/
 export const TOGGLE_LIMIT_TO_PAYMARKET  = '[Features/Peer/Exchange Explorer] Toggle Limit To PayMarket';
 export const TOGGLE_EXCLUDE_INDIRECT_JOB_MATCHES  = '[Features/Peer/Exchange Explorer] Toggle Exclude Indirect Job Matches';
 export const TOGGLE_INCLUDE_UNTAGGED_EMPLOYEES = '[Features/Peer/Exchange Explorer] Toggle Include Untagged Employees';
-export const LIMIT_TO_EXCHANGE = '[Features/Peer/Exchange Explorer] Limit to Exchange';
 export const SET_EXCHANGE_SCOPE_SELECTION = '[Features/Peer/Exchange Explorer] Set Scope Selection';
 export const CLEAR_EXCHANGE_SCOPE_SELECTION = '[Features/Peer/Exchange Explorer] Clear Scope Selection';
 export const SET_FILTER_CONTEXT = '[Features/Peer/Exchange Explorer] Set Filter Context';
@@ -23,12 +22,6 @@ export class ToggleExcludeIndirectJobMatches implements Action {
 
 export class ToggleIncludeUntaggedEmployees implements Action {
   readonly type = TOGGLE_INCLUDE_UNTAGGED_EMPLOYEES;
-}
-
-export class LimitToExchange implements Action {
-  readonly type = LIMIT_TO_EXCHANGE;
-
-  constructor(public payload: number) { }
 }
 
 export class SetExchangeScopeSelection implements Action {
@@ -69,7 +62,6 @@ export type Actions
   = ToggleLimitToPayMarket
   | ToggleExcludeIndirectJobMatches
   | ToggleIncludeUntaggedEmployees
-  | LimitToExchange
   | SetExchangeScopeSelection
   | SetFilterContext
   | SetFilterContextSilently
