8d38b275aefdca04025c8e9019603f7666b01bb6
deepabalasundaram <deepa.balasundaram@payfactors.com>
2020-03-02 13:26:49 -0500

    ARCH-174: NG to Client Migration: Update NG links to use JDMCoreUseClient setting

--- apps/job-description-management/app/_settings/layout/layout.page/layout.page.ts
+++ apps/job-description-management/app/_settings/layout/layout.page/layout.page.ts
@@ -1,42 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { Observable, Subscription } from 'rxjs';
-
-import { SettingsService } from 'libs/state/app-context/services';
-import { CompanySettingsEnum } from 'libs/models';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pf-settings-layout-page',
   templateUrl: './layout.page.html',
   styleUrls: ['./layout.page.scss']
 })
-export class LayoutPageComponent implements OnInit, OnDestroy {
+export class LayoutPageComponent {
   navLinks = [
     {name: 'Job Description Views', route: '/settings/job-description-views' },
     {name: 'Routing Workflows', route: '/settings/workflows' },
     {name: 'Manage Company Controls', route: '/settings/company-controls' },
     {name: 'Footer View', route: '/settings/jdm-footer-view' }
   ];
-  enableCoreJdmInClient$: Observable<boolean>;
-  enableCoreJdmInClientSubscription: Subscription;
 
-  jdmUrl = '/ng/job-description-management/job-descriptions';
-
-  constructor(private settingService: SettingsService) {
-    this.enableCoreJdmInClient$ = this.settingService.selectCompanySetting<boolean>(
-      CompanySettingsEnum.JDMCoreUseClient
-    );
-  }
-
-  ngOnInit() {
-    this.enableCoreJdmInClientSubscription = this.enableCoreJdmInClient$.subscribe((setting) => {
-      if (setting === true) {
-        this.jdmUrl = '/client/job-description-management/templates';
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    this.enableCoreJdmInClientSubscription.unsubscribe();
-  }
+  constructor() {}
 }
