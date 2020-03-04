840bb33ff4d282402133eee56c3b2ab4afb2e159
Mattias Andries <mattias.andries@payfactors.com>
2020-03-04 15:28:50 -0500

    SUP-323 Support UDF_1 in entity keys

--- apps/data-management/app/_main/models/entity-identifiers-view.model.ts
+++ apps/data-management/app/_main/models/entity-identifiers-view.model.ts
@@ -2,23 +2,24 @@ import { FieldNames } from './';
 
 export interface EntityIdentifierViewModel {
     Field: string;
+    DbColumn: string;
     isChecked: boolean;
     isDisabled: boolean;
 }
 
 export function EntityIdentifierViewModelOptions(): EntityIdentifierViewModel[] {
     return [
-        { Field: FieldNames.EMPLOYEE_ID, isChecked: true, isDisabled: true },
-        { Field: FieldNames.DEPARTMENT, isChecked: false, isDisabled: false },
-        { Field: FieldNames.MANAGER_EMPLOYEE_ID, isChecked: false, isDisabled: false },
-        { Field: FieldNames.JOB_CODE, isChecked: false, isDisabled: false }
+        { Field: FieldNames.EMPLOYEE_ID, DbColumn: FieldNames.EMPLOYEE_ID, isChecked: true, isDisabled: true },
+        { Field: FieldNames.DEPARTMENT, DbColumn: FieldNames.DEPARTMENT, isChecked: false, isDisabled: false },
+        { Field: FieldNames.MANAGER_EMPLOYEE_ID, DbColumn: FieldNames.MANAGER_EMPLOYEE_ID, isChecked: false, isDisabled: false },
+        { Field: FieldNames.JOB_CODE, DbColumn: FieldNames.JOB_CODE, isChecked: false, isDisabled: false }
     ];
 }
 
 export function MockEntityIdentifierViewModelOptions(): EntityIdentifierViewModel[] {
     return [
         ...EntityIdentifierViewModelOptions(),
-        { Field: 'ExtraField1', isChecked: false, isDisabled: false },
-        { Field: 'ExtraField2', isChecked: false, isDisabled: false }
+        { Field: 'ExtraField1', DbColumn: 'ExtraField1', isChecked: false, isDisabled: false },
+        { Field: 'ExtraField2', DbColumn: 'ExtraField2', isChecked: false, isDisabled: false }
     ];
 }
