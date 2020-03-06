dc115e35378a9b5c8542ca976b5b72787e391a3d
Mattias Andries <mattias.andries@payfactors.com>
2020-03-06 10:34:20 -0500

    SUP-348 Show UDF_1 as disabled if not defined

--- apps/data-management/app/_main/models/constants.ts
+++ apps/data-management/app/_main/models/constants.ts
@@ -4,4 +4,5 @@ export class FieldNames {
     public static DEPARTMENT = 'Department';
     public static MANAGER_EMPLOYEE_ID = 'Manager_Employee_ID';
     public static JOB_CODE = 'Job_Code';
+    public static UDF_CHAR_1 = 'UDF_CHAR_1';
 }
