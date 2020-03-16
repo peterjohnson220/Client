ff7040626f344cc0904ce4c1583d90d2f9b897cc
Greg LeClair <greg.leclair@payfactors.com>
2020-03-16 11:26:33 -0400

    ENG-260

--- apps/jobs/app/_jobs-page/components/jobs-details/jobs-details.component.ts
+++ apps/jobs/app/_jobs-page/components/jobs-details/jobs-details.component.ts
@@ -96,7 +96,11 @@ export class JobsDetailsComponent implements OnDestroy, OnInit {
         if (notPricedData.total > 0) {
           this.pricedCount = notPricedData.data[0]['vw_UnpricedJobPayMarketMetadata_PricingCount'];
         } else if (this.pricedDataObj) {
-          this.pricedCount = this.payMarketCount - this.pricedDataObj.data[0]['CompanyJobs_Pricings_NotPricedCount'];
+          if (this.pricedDataObj.total > 0) {
+            this.pricedCount = this.payMarketCount - this.pricedDataObj.data[0]['CompanyJobs_Pricings_NotPricedCount'];
+          } else {
+            this.pricedCount = this.payMarketCount - this.notPricedCount;
+          }
         } else {
           this.pricedCount = this.payMarketCount;
         }
@@ -109,7 +113,11 @@ export class JobsDetailsComponent implements OnDestroy, OnInit {
         if (pricedData.total > 0) {
           this.notPricedCount = pricedData.data[0]['CompanyJobs_Pricings_NotPricedCount'];
         } else if (this.notPricedDataObj) {
-          this.notPricedCount = this.payMarketCount - this.notPricedDataObj.data[0]['vw_UnpricedJobPayMarketMetadata_PricingCount'];
+          if (this.notPricedDataObj.total > 0) {
+            this.notPricedCount = this.payMarketCount - this.notPricedDataObj.data[0]['vw_UnpricedJobPayMarketMetadata_PricingCount'];
+          } else {
+            this.notPricedCount = this.payMarketCount - this.pricedCount;
+          }
         } else {
           this.notPricedCount = this.payMarketCount;
         }
@@ -159,5 +167,7 @@ export class JobsDetailsComponent implements OnDestroy, OnInit {
     this.notPricedDataSubscription.unsubscribe();
     this.pricedDataSubscription.unsubscribe();
     this.companyPayMarketsSubscription.unsubscribe();
+
+    this.store.dispatch(new fromJobsPageActions.ChangePricingDetailsView('Priced'));
   }
 }
