076667f24db5e0991b36fc95bfed4ba1f7191c66
Brandon Collins <brandon.collins@payfactors.com>
2020-04-16 15:11:36 -0400

    STRUC-263: Outliers don't get updated until page is refreshed

--- apps/structures/app/_job-based-range/shared/effects/shared.effects.ts
+++ apps/structures/app/_job-based-range/shared/effects/shared.effects.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Action, Store } from '@ngrx/store';
+import { Action, select, Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
-import { catchError, switchMap, mergeMap, map } from 'rxjs/operators';
+import { catchError, switchMap, mergeMap, map, withLatestFrom } from 'rxjs/operators';
 
 import { StructureModelingApiService } from 'libs/data/payfactors-api/structures';
+import { DataViewApiService } from 'libs/data/payfactors-api/reports';
+import { DataGridToDataViewsHelper } from 'libs/features/pf-data-grid/helpers';
 import * as pfDataGridActions from 'libs/features/pf-data-grid/actions';
+import * as fromPfDataGridReducer from 'libs/features/pf-data-grid/reducers';
+import * as fromPfDataGridActions from 'libs/features/pf-data-grid/actions';
+import { DataViewFilter } from 'libs/models/payfactors-api/reports/request';
 
 import * as fromSharedActions from '../actions/shared.actions';
 import * as fromSharedReducer from '../reducers';
 import { PageViewIds } from '../constants/page-view-ids';
-import { ModelDataColumns, ModelGridColumns } from '../constants/model-grid-mappings';
 import { PayfactorsApiModelMapper } from '../helpers/payfactors-api-model-mapper';
 import { Pages } from '../constants/pages';
 
@@ -22,25 +25,64 @@ export class SharedEffects {
   updateMid$: Observable<Action> = this.actions$
     .pipe(
       ofType(fromSharedActions.UPDATE_MID),
-      switchMap((action: fromSharedActions.UpdateMid) => {
-          return this.structureModelingApiService.recalculateRangeMinMax(PayfactorsApiModelMapper.mapUpdateRangeInputToRecalcAndSaveRangeMinMaxRequest(
-            action.payload.RangeGroupId, action.payload.RangeId, action.payload.Mid, action.payload.RowIndex, action.payload.RoundingSettings)).pipe(
-            mergeMap((response) => {
-              const actions = [];
-              actions.push(new pfDataGridActions.UpdateRow(PageViewIds.Model, response.RowIndex,
-                [{gridName: ModelGridColumns.Min, dataName: ModelDataColumns.Min},
-                  {gridName: ModelGridColumns.Max, dataName: ModelDataColumns.Max},
-                  {gridName: ModelGridColumns.Mid, dataName: ModelDataColumns.Mid}], response.Range));
+      switchMap(
+        (action: fromSharedActions.UpdateMid) =>
+          this.structureModelingApiService.recalculateRangeMinMax(PayfactorsApiModelMapper.mapUpdateRangeInputToRecalcAndSaveRangeMinMaxRequest(
+            action.payload.RangeGroupId,
+            action.payload.RangeId,
+            action.payload.Mid,
+            action.payload.RowIndex,
+            action.payload.RoundingSettings))
+        .pipe(
+          mergeMap((response) => {
+            const actions = [];
 
-              if (action.payload.Page === Pages.Employees) {
-                actions.push(new pfDataGridActions.LoadData(PageViewIds.Employees));
-              }
-              return actions;
-            }),
-            catchError(response => of(new fromSharedActions.UpdateMidError()))
-          );
-        }
-      ));
+            actions.push(new fromSharedActions.UpdateMidSuccess({ rangeId: action.payload.RangeId, rowIndex: action.payload.RowIndex }));
+
+            if (action.payload.Page === Pages.Employees) {
+              actions.push(new pfDataGridActions.LoadData(PageViewIds.Employees));
+            }
+            return actions;
+          }),
+          catchError(() => of(new fromSharedActions.UpdateMidError()))
+        )
+      )
+    );
+
+  @Effect()
+  updateMidSuccess$ = this.actions$
+    .pipe(
+      ofType(fromSharedActions.UPDATE_MID_SUCCESS),
+      withLatestFrom(
+        this.store.pipe(select(fromPfDataGridReducer.getBaseEntity, PageViewIds.Model)),
+        this.store.pipe(select(fromPfDataGridReducer.getFields, PageViewIds.Model)),
+        this.store.pipe(select(fromPfDataGridReducer.getPagingOptions, PageViewIds.Model)),
+        this.store.pipe(select(fromPfDataGridReducer.getSortDescriptor, PageViewIds.Model)),
+        this.store.pipe(select(fromPfDataGridReducer.getApplyDefaultFilters, PageViewIds.Model)),
+        (action: fromSharedActions.UpdateMidSuccess, baseEntity, fields, pagingOptions, sortDescriptor, applyDefaultFilters) =>
+          ({ action, baseEntity, fields, pagingOptions, sortDescriptor, applyDefaultFilters })
+      ),
+      switchMap((data) => {
+        const filter: DataViewFilter = {
+          EntitySourceName: 'CompanyStructures_Ranges',
+          SourceName: 'CompanyStructuresRanges_ID',
+          Operator: '=',
+          Values: [data.action.payload.rangeId.toString()]
+        };
+
+        return this.dataViewApiService.getData(DataGridToDataViewsHelper.buildDataViewDataRequest(
+          data.baseEntity.Id,
+          data.fields,
+          [...DataGridToDataViewsHelper.mapFieldsToFiltersUseValuesProperty(data.fields), filter],
+          { From: 0, Count: 1},
+          data.sortDescriptor,
+          false,
+          false
+        )).pipe(
+          map((response) => new fromPfDataGridActions.UpdateRow(PageViewIds.Model, data.action.payload.rowIndex, response[0]))
+        );
+      })
+    );
 
   @Effect()
   recalculateRangesWithoutMid$: Observable<Action> = this.actions$
@@ -61,8 +103,8 @@ export class SharedEffects {
   constructor(
     private actions$: Actions,
     private structureModelingApiService: StructureModelingApiService,
-    private router: Router,
-    private store: Store<fromSharedReducer.State>
+    private store: Store<fromSharedReducer.State>,
+    private dataViewApiService: DataViewApiService
   ) {
   }
 }
