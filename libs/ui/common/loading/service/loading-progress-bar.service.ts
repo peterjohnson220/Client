dd3a61d6fd056c4a19e3810c324bdeb3077699fa
Daniel Cardenas <daniel.cardenas@payfactors.com>
2020-04-27 11:07:55 -0400

    SUP-413 Progress bar animation not starting on consecutive loads

--- libs/ui/common/loading/service/loading-progress-bar.service.ts
+++ libs/ui/common/loading/service/loading-progress-bar.service.ts
@@ -5,40 +5,39 @@ import {LoadingProgressBarModel} from '../models';
 
 @Injectable()
 export class LoadingProgressBarService {
-  private _progress = new Subject<number>();
+  private _progress: Subject<number>;
   private _stop = false;
   progressBar(): Observable<number> {
+    this._progress = new Subject<number>();
     return this._progress.asObservable();
   }
 
   start(loadingProgress: LoadingProgressBarModel): void {
+    this._stop = false;
     let i =  0;
-    if (!this._stop) {
-      timer(0, 1000).pipe(
-        takeWhile(() => !this._stop),
-        map(() => {
-          let value;
-          if (i === loadingProgress.interval + 1) {
-            i = 0;
-            return i;
-          }
-          value = i !== 0 ? i * loadingProgress.intervalValue : i;
-          i++;
-          if (loadingProgress.interval - 1 === i && !loadingProgress.progressive) {
-            this._stop = true;
-          }
-          return value;
-        })
-      ).subscribe({
-        next: t => this._progress.next(t),
-        error: null,
-        complete: () => {
-          this._progress.complete();
-          this._progress = new Subject<number>();
-          this._stop = false;
+    timer(0, 1000).pipe(
+      takeWhile(() => !this._stop),
+      map(() => {
+        let value;
+        if (i === loadingProgress.interval + 1) {
+          i = 0;
+          return i;
         }
-      });
-    }
+        value = i !== 0 ? i * loadingProgress.intervalValue : i;
+        i++;
+        if (loadingProgress.interval - 1 === i && !loadingProgress.progressive) {
+          this._stop = true;
+        }
+        return value;
+      })
+    ).subscribe({
+      next: t => this._progress.next(t),
+      error: null,
+      complete: () => {
+        this._progress.complete();
+        this._progress = new Subject<number>();
+      }
+    });
   }
 
   stop(stop: boolean): void {
