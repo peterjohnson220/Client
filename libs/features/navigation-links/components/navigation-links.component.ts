0a5abee8d5ba02295f4e2bd523d9859211eb5ba6
Mattias Andries <mattias.andries@payfactors.com>
2020-05-28 15:17:09 -0400

    SUP-437 send first notification from client

--- libs/features/navigation-links/components/navigation-links.component.ts
+++ libs/features/navigation-links/components/navigation-links.component.ts
@@ -5,6 +5,9 @@ import { Store } from '@ngrx/store';
 
 import * as fromRootState from 'libs/state/state';
 import * as fromOrgDataNavigationLinkActions from 'libs/features/navigation-links/actions/org-data-navigation-link.actions';
+import * as fromAppNotificationsMainReducer from '../../app-notifications/reducers';
+import * as fromAppNotificationsActions from '../../app-notifications/actions/app-notifications.actions';
+import { NotificationLevel, NotificationSource, NotificationType } from '../../app-notifications/models';
 
 @Component({
     selector: 'pf-navigation-links',
@@ -15,13 +18,25 @@ export class NavigationLinksComponent implements OnInit {
 
     @Input() navigationGroupLinks: NavigationLinkGroup[];
 
-    constructor(private store: Store<fromRootState.State>) { }
+    constructor(private store: Store<fromRootState.State>,
+                private notificationStore: Store<fromAppNotificationsMainReducer.State>) { }
     ngOnInit() { }
 
   handleClick($event) {
     const linkName = $event.target.innerText;
 
     if (linkName === 'Download Organizational Data') {
+      const notification = {
+        NotificationId: '',
+        Level: NotificationLevel.Info,
+        From: NotificationSource.GenericNotificationMessage,
+        Payload: {
+          Title: 'Please wait while your file is built'
+        },
+        EnableHtml: true,
+        Type: NotificationType.Event
+      };
+      this.notificationStore.dispatch(new fromAppNotificationsActions.AddNotification(notification));
       this.store.dispatch(new fromOrgDataNavigationLinkActions.InitiateOrgDataExport());
       $event.preventDefault();
     }
