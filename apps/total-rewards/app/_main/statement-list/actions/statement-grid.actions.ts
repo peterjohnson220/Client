cd496f476b740ce483a73fe276890eaf736caf6c
Adam Colson <Adam.Colson@payfactors.com>
2020-06-03 13:57:06 -0400

    TS-201 TRS delete statement from list page grid > action menu

--- apps/total-rewards/app/_main/statement-list/actions/statement-grid.actions.ts
+++ apps/total-rewards/app/_main/statement-list/actions/statement-grid.actions.ts
@@ -1,6 +1,8 @@
 import { Action } from '@ngrx/store';
 import { GridDataResult } from '@progress/kendo-angular-grid';
 
+import { StatementListViewModel } from '../../../shared/models';
+
 export const LOAD_STATEMENTS = '[Total Rewards/Statement List/Statement Grid] Load Statements';
 export const LOAD_STATEMENTS_SUCCESS = '[Total Rewards/Statements/Statement Grid] Load Statements Success';
 export const LOAD_STATEMENTS_ERROR = '[Total Rewards/Statements/Statement Grid] Load Statements Error';
@@ -8,6 +10,12 @@ export const UPDATE_SEARCH_TERM = '[Total Rewards/Statements/Statement Grid] Upd
 
 export const OPEN_ACTION_MENU = '[Total Rewards/Statements] Open Action Menu';
 export const CLOSE_ACTION_MENU = '[Total Rewards/Statements] Close Action Menu';
+
+export const CONFIRM_DELETE_STATEMENT = '[Total Rewards/Statements] Confirm Delete Statement';
+export const CLOSE_DELETE_STATEMENT = '[Total Rewards/Statements] Close Delete Statement';
+export const DELETE_STATEMENT = '[Total Rewards/Statements] Delete Statement';
+export const DELETE_STATEMENT_SUCCESS = '[Total Rewards/Statements] Delete Statement Success';
+export const DELETE_STATEMENT_ERROR = '[Total Rewards/Statements] Delete Statement Error';
 
 export class LoadStatements implements Action {
   readonly type = LOAD_STATEMENTS;
@@ -29,11 +37,31 @@ export class UpdateSearchTerm implements Action {
 
 export class OpenActionMenu implements Action {
   readonly type = OPEN_ACTION_MENU;
-  constructor(public payload: string) { }
+  constructor(public payload: StatementListViewModel) { }
 }
 
 export class CloseActionMenu implements Action {
   readonly type = CLOSE_ACTION_MENU;
+}
+
+export class ConfirmDeleteStatement implements Action {
+  readonly type = CONFIRM_DELETE_STATEMENT;
+}
+
+export class CloseDeleteStatement implements Action {
+  readonly type = CLOSE_DELETE_STATEMENT;
+}
+
+export class DeleteStatement implements Action {
+  readonly type = DELETE_STATEMENT;
+}
+
+export class DeleteStatementSuccess implements Action {
+  readonly type = DELETE_STATEMENT_SUCCESS;
+}
+
+export class DeleteStatementError implements Action {
+  readonly type = DELETE_STATEMENT_ERROR;
 }
 
 export type StatementGridActions =
@@ -42,4 +70,9 @@ export type StatementGridActions =
   LoadStatementsError |
   UpdateSearchTerm |
   OpenActionMenu |
-  CloseActionMenu;
+  CloseActionMenu |
+  ConfirmDeleteStatement |
+  CloseDeleteStatement |
+  DeleteStatement |
+  DeleteStatementSuccess |
+  DeleteStatementError;
