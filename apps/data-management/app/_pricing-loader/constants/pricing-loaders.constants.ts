ea5280f5e8a4868e7c210844cbd9cdacf4209fa0
Daniel Cardenas <daniel.cardenas@payfactors.com>
2020-06-04 11:33:55 -0400

    SUP-393   Add validation to Match Weight and Composite Adjustment

--- apps/data-management/app/_pricing-loader/constants/pricing-loaders.constants.ts
+++ apps/data-management/app/_pricing-loader/constants/pricing-loaders.constants.ts
@@ -2,9 +2,14 @@ import {MrpFieldsModel} from '../models/mrp-fields.model';
 
 export const FILETYPES: string [] = ['.CSV', '.XLSX'];
 export const MRPFIELDS: MrpFieldsModel [] [] = [
-  [{ Name: 'Base', Value: 50, Min: 1, Max: 100}, { Name: 'TDC', Value: 50, Min: 1, Max: 100}, { Name: 'Bonus %', Value: 50, Min: 1, Max: 100}, { Name: 'Match Weight', Value: 1, Min: 1, Max: 100}],
-  [{ Name: 'TCC', Value: 50, Min: 1, Max: 100}, { Name: 'Allow', Value: 50, Min: 1, Max: 100}, { Name: 'Bonus Target', Value: 50, Min: 1, Max: 100}, { Name: 'Composite  Adj', Value: 0, Min: 0, Max: 100}],
-  [{ Name: 'Bonus', Value: 50, Min: 1, Max: 100}, { Name: 'Fixed', Value: 50, Min: 1, Max: 100}, { Name: 'Bonus Target %', Value: 50, Min: 1, Max: 100}],
-  [{ Name: 'TCC Target', Value: 50, Min: 1, Max: 100}, { Name: 'Renum', Value: 50, Min: 1, Max: 100}, { Name: 'Target LTIP', Value: 50, Min: 1, Max: 100}],
-  [{ Name: 'LTIP', Value: 50, Min: 1, Max: 100}, { Name: 'TGP', Value: 50, Min: 1, Max: 100}, { Name: 'Target TDC', Value: 50, Min: 1, Max: 100}]
+  [{ Name: 'Base', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'TDC', Value: 50, Min: 1, Max: 100, Decimal: 0},
+    { Name: 'Bonus %', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'Match Weight', Value: 1, Min: 0.01, Max: 100, Decimal: 2}],
+  [{ Name: 'TCC', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'Allow', Value: 50, Min: 1, Max: 100, Decimal: 0},
+    { Name: 'Bonus Target', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'Composite  Adj', Value: 0, Min: -100, Max: 100, Decimal: 2}],
+  [{ Name: 'Bonus', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'Fixed', Value: 50, Min: 1, Max: 100, Decimal: 0},
+    { Name: 'Bonus Target %', Value: 50, Min: 1, Max: 100, Decimal: 0}],
+  [{ Name: 'TCC Target', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'Renum', Value: 50, Min: 1, Max: 100, Decimal: 0},
+    { Name: 'Target LTIP', Value: 50, Min: 1, Max: 100, Decimal: 0}],
+  [{ Name: 'LTIP', Value: 50, Min: 1, Max: 100, Decimal: 0}, { Name: 'TGP', Value: 50, Min: 1, Max: 100, Decimal: 0},
+    { Name: 'Target TDC', Value: 50, Min: 1, Max: 100, Decimal: 0}]
 ];
