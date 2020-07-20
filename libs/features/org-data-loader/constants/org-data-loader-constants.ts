badb28255fa06d71653adcebf1c6c182a81072a1
Mattias Andries <mattias.andries@payfactors.com>
2020-07-20 10:48:59 -0400

    SUP-405 Add Structure Code and Grade Code to field mapper

--- libs/features/org-data-loader/constants/org-data-loader-constants.ts
+++ libs/features/org-data-loader/constants/org-data-loader-constants.ts
@@ -85,7 +85,9 @@ export const ORG_DATA_PF_EMPLOYEE_FIELDS = [
   'Ethnicity_PF',
   'DateOfEntry_PF',
   'STIElig',
-  'LTIElig'
+  'LTIElig',
+  'Structure_Code',
+  'Grade_Code'
 ];
 
 export const DATE_FORMATS: DateFormatItem[] = [
