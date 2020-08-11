fe22296cff1422b779d398517d261dda5ec9ed47
hhuynhpf <hung.huynh@payfactors.com>
2020-08-11 12:04:12 -0400

    IDEA-845

--- apps/total-rewards/app/shared/fa-icons.ts
+++ apps/total-rewards/app/shared/fa-icons.ts
@@ -4,9 +4,9 @@ import { faDollarSign, faPlus, faCopy, faTrash, faTrashAlt, faFileAlt, faPlay, f
 
 import { faInfoCircle } from '@fortawesome/pro-light-svg-icons';
 
-import { faTrophyAlt } from '@fortawesome/pro-regular-svg-icons/faTrophyAlt';
+import { faTrophyAlt, faLongArrowDown, faLongArrowUp  } from '@fortawesome/pro-regular-svg-icons';
 
 export const faIcons = [
   faDollarSign, faPlus, faCopy, faFileAlt, faTrash, faTrashAlt, faPlay, faHistory, faCheck, faPencil, faEllipsisH, faCog, faTimes,
-  faImage, faSearch, faChevronLeft, faInfoCircle, faSlidersH, faUserSlash, faTrophyAlt, faDownload
+  faImage, faSearch, faChevronLeft, faInfoCircle, faSlidersH, faUserSlash, faTrophyAlt, faDownload, faLongArrowDown, faLongArrowUp
 ];
