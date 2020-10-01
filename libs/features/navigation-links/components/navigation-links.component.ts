f2eccaa08f512aaae9095999aca14fe646444de1
dcardenas85 <54075606+dcardenas85@users.noreply.github.com>
2020-10-01 12:39:56 -0400

    Revert "SUP-604 IE11 not redirecting user to pricing export page"

--- libs/features/navigation-links/components/navigation-links.component.ts
+++ libs/features/navigation-links/components/navigation-links.component.ts
@@ -46,7 +46,7 @@ export class NavigationLinksComponent implements OnInit, OnDestroy {
 
   handleClick($event) {
     const linkName = $event.target.innerText;
-    switch (linkName.trim()) {
+    switch (linkName) {
       case 'Download Organizational Data': {
           const notification = {
             NotificationId: '',
