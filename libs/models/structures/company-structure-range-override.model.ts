ddb054866c9fb0bb3c3c446a3216bac37e83a232
vitaly_zisserman <vitaly.zisserman@payfactors.com>
2020-10-20 10:54:26 -0400

    STRUC-445: Add increase by % for jobs with no market data

--- libs/models/structures/company-structure-range-override.model.ts
+++ libs/models/structures/company-structure-range-override.model.ts
@@ -14,4 +14,5 @@ export interface CompanyStructureRangeOverride {
   MidForcedToCurrent: boolean;
   UsePublishedRange: boolean;
   MidForcedToCurrentPercent: boolean;
+  IncreaseCurrentByPercent: boolean;
 }
