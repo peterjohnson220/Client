00a8258da7f31d8e5cab3608c77bd7a22209bd32
Corey Weiner <corey.weiner@payfactors.com>
2020-12-10 09:12:56 -0500

    HRINT-467 > move setting service into component instead of effect

--- apps/data-management/app/_main/effects/load-and-export-files-card.effects.ts
+++ apps/data-management/app/_main/effects/load-and-export-files-card.effects.ts
@@ -2,12 +2,10 @@ import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Observable, of } from 'rxjs';
-import { catchError, map, withLatestFrom } from 'rxjs/operators';
+import { catchError, switchMap} from 'rxjs/operators';
 
 import { PermissionService } from 'libs/core/services/permission.service';
-import { SettingsService } from 'libs/state/app-context/services';
 import { Permissions, PermissionCheckEnum } from 'libs/constants';
-import { CompanySettingsEnum } from 'libs/models';
 
 import * as fromLoadAndExportFilesCardActions from '../actions/load-and-export-files-card.actions';
 import { LoadAndExportFilesCardState } from '../reducers/load-and-export-files-card.reducer';
@@ -17,18 +15,8 @@ export class LoadAndExportFilesCardEffects {
   @Effect()
   initLoadAndExportFilesCard$: Observable<Action> = this.actions$.pipe(
     ofType<fromLoadAndExportFilesCardActions.InitLoadAndExportFilesCard>(fromLoadAndExportFilesCardActions.INIT_LOAD_AND_EXPORT_FILES_CARD),
-    withLatestFrom(
-      this.settingsService.selectCompanySetting<string>(CompanySettingsEnum.ManualOrgDataLoadLink, 'string'),
-      (action: fromLoadAndExportFilesCardActions.InitLoadAndExportFilesCard,
-        manualOrgDataLoadLinkSetting) => {
-        return {
-          action,
-          manualOrgDataLoadLinkSetting
-        };
-      }),
-    map((obj) => {
+    switchMap((obj) => {
       const cardState: LoadAndExportFilesCardState = {
-        canImportOrgData: obj.manualOrgDataLoadLinkSetting === 'true',
         canExportOrgData: this.permissionService.CheckPermission([Permissions.CAN_DOWNLOAD_ORGANIZATIONAL_DATA], PermissionCheckEnum.Single),
         canExportPricingData: this.permissionService.CheckPermission([Permissions.CAN_DOWNLOAD_PRICING_DATA], PermissionCheckEnum.Single),
         canExportJobDescription: this.permissionService.CheckPermission([Permissions.CAN_DOWNLOAD_JOB_DESCRIPTION_DATA], PermissionCheckEnum.Single),
@@ -36,14 +24,13 @@ export class LoadAndExportFilesCardEffects {
         loading: false
       };
 
-      return new fromLoadAndExportFilesCardActions.InitLoadAndExportFilesCardSuccess(cardState);
+      return [new fromLoadAndExportFilesCardActions.InitLoadAndExportFilesCardSuccess(cardState)];
     }),
     catchError((e) => of(new fromLoadAndExportFilesCardActions.InitLoadAndExportFilesCardError()))
   );
 
   constructor(
     private actions$: Actions,
-    private permissionService: PermissionService,
-    private settingsService: SettingsService
+    private permissionService: PermissionService
   ) {}
 }
