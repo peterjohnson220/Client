f0013b3a05e27c8cfb42642851de5060b57e80dc
James <james.prehemo@payfactors.com>
2020-12-13 13:41:50 -0500

    PEER-593 - Show Notification When Items are Added to the Resources Tile

--- libs/features/user-notifications/constants/user-notification-constants.ts
+++ libs/features/user-notifications/constants/user-notification-constants.ts
@@ -4,4 +4,5 @@ export class UserNotificationConstants {
   static readonly ROUTED_JOB_DESCRIPTION_LOOKUP_KEY = 'RoutedJobDescription';
   static readonly JOB_DESCRIPTION_REVIEW_COMPLETE_LOOKUP_KEY = 'JobDescriptionReviewComplete';
   static readonly JOB_DESCRIPTION_REVIEW_REJECTED_LOOKUP_KEY = 'JobDescriptionReviewRejected';
+  static readonly COMPANY_RESOURCES_LOOKUP_KEY = 'CompanyResources';
 }
