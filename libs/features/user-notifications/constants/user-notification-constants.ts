c63670cf23a6f8b854757108f563e8ef29a119d0
Patrick Bailey <patrick.bailey@payfactors.com>
2020-12-14 09:31:11 -0500

    Adds user notification for job description review completion

--- libs/features/user-notifications/constants/user-notification-constants.ts
+++ libs/features/user-notifications/constants/user-notification-constants.ts
@@ -2,4 +2,5 @@
 export class UserNotificationConstants {
   static readonly USER_TICKET_LOOKUP_KEY = 'UserTicket';
   static readonly ROUTED_JOB_DESCRIPTION_LOOKUP_KEY = 'RoutedJobDescription';
+  static readonly JOB_DESCRIPTION_REVIEW_COMPLETE_LOOKUP_KEY = 'JobDescriptionReviewComplete';
 }
--- libs/features/user-notifications/containers/user-notification-container/user-notification-container.component.ts
+++ libs/features/user-notifications/containers/user-notification-container/user-notification-container.component.ts
@@ -3,6 +3,9 @@ import { Component, ComponentFactory, ComponentFactoryResolver, Input, OnInit, V
 import { UserTicketNotificationComponent } from '../categories/user-ticket/user-ticket-notification.component';
 import { UserNotificationBaseComponent } from '../user-notification-base/user-notification-base.component';
 import { RoutedJobDescriptionNotificationComponent } from '../categories/routed-job-description/routed-job-description-notification.component';
+import {
+  JobDescriptionReviewCompleteNotificationComponent
+} from '../categories/job-description-review-complete/job-description-review-complete-notification.component';
 import { UserNotification } from '../../models';
 import { UserNotificationHostDirective } from '../../directives';
 import { UserNotificationConstants } from '../../constants';
@@ -32,6 +35,10 @@ export class UserNotificationContainerComponent  implements OnInit {
 
       case UserNotificationConstants.ROUTED_JOB_DESCRIPTION_LOOKUP_KEY:
         componentFactory = this.componentFactoryResolver.resolveComponentFactory(RoutedJobDescriptionNotificationComponent);
+        break;
+
+      case UserNotificationConstants.JOB_DESCRIPTION_REVIEW_COMPLETE_LOOKUP_KEY:
+        componentFactory = this.componentFactoryResolver.resolveComponentFactory(JobDescriptionReviewCompleteNotificationComponent);
         break;
 
       default:
