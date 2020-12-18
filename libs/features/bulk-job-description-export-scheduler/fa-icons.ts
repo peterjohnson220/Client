5c45170afde9dfc5a337c2eb140fc0bfab1a52d6
Corey Weiner <corey.weiner@payfactors.com>
2020-12-18 11:52:11 -0500

    HRINT-461 > update UI for bulk jobs export scheduler

--- libs/features/bulk-job-description-export-scheduler/fa-icons.ts
+++ libs/features/bulk-job-description-export-scheduler/fa-icons.ts
@@ -1,4 +1,6 @@
 import { faTrash } from '@fortawesome/pro-solid-svg-icons';
 import { faTrashAlt } from '@fortawesome/pro-solid-svg-icons/faTrashAlt';
+import { faCaretDown } from '@fortawesome/pro-solid-svg-icons/faCaretDown';
+import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
 
-export const faIcons = [ faTrash, faTrashAlt];
+export const faIcons = [ faTrash, faTrashAlt, faCaretDown, faCaretRight];
