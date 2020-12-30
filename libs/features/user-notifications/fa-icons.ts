192c06cfe5b9a0b3c63bbcbf23a96757ebbda12a
Peter Johnson <peter.johnson@payfactors.com>
2020-12-30 09:15:12 -0500

    PEER-590.1: PendingPeerJobMatch UserNotifications - fixing icon and grammer

--- libs/features/user-notifications/fa-icons.ts
+++ libs/features/user-notifications/fa-icons.ts
@@ -1,5 +1,6 @@
-import { faCircle } from '@fortawesome/pro-solid-svg-icons/faCircle';
-import { faQuestionSquare as farQuestionSquare, faFileAlt as farFileAlt} from '@fortawesome/pro-regular-svg-icons';
+import { faCircle, faExchangeAlt as fasExchangeAlt } from '@fortawesome/pro-solid-svg-icons';
+import { faQuestionSquare as farQuestionSquare, faFileAlt as farFileAlt, faExchangeAlt as farExchangeAlt } from '@fortawesome/pro-regular-svg-icons';
 import { faNewspaper as falNewspaper, faCommentAltDots as falCommentAltDots } from '@fortawesome/pro-light-svg-icons';
 
-export const faIcons = [faCircle, farQuestionSquare, farFileAlt, falNewspaper, falCommentAltDots];
+
+export const faIcons = [faCircle, farQuestionSquare, farFileAlt, falNewspaper, falCommentAltDots, farExchangeAlt, fasExchangeAlt];
