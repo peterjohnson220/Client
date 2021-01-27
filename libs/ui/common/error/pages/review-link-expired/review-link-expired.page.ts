ee087907c743efd9e4c28a1baaad8b5b7f32e089
Patrick Bailey <patrick.bailey@payfactors.com>
2021-01-27 15:36:45 -0500

    PEER-691 - Update Page when JDM Review Link is Expired

---
+++ libs/ui/common/error/pages/review-link-expired/review-link-expired.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pf-review-link-expired-page',
+  templateUrl: 'review-link-expired.page.html',
+  styleUrls: ['review-link-expired.page.scss']
+})
+
+export class ReviewLinkExpiredPageComponent {}
