ca61ec0c0e3780d9f3ccf2e0c7fb909d0a4249cd
Patrick Bailey <patrick.bailey@payfactors.com>
2021-02-16 15:19:23 -0500

    PEER-740.1 - Closes tooltip when editing custom Peer scope starts to prevent it from moving to the upper left of the modal

--- libs/features/pricings/multi-match/components/job-match-cut-title/job-match-cut-title.component.ts
+++ libs/features/pricings/multi-match/components/job-match-cut-title/job-match-cut-title.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter, ViewChild} from '@angular/core';
+import { TooltipDirective } from '@progress/kendo-angular-tooltip';
 import { JobMatchCut } from 'libs/models/payfactors-api';
 import { PricingMatchEntityTypes } from 'libs/constants';
 import { JobToPrice } from '../../models';
@@ -16,12 +17,16 @@ export class JobMatchCutTitleComponent implements OnInit {
   @Output() editCut: EventEmitter<{ jobCut: JobMatchCut, job: JobToPrice }>
     = new EventEmitter<{ jobCut: JobMatchCut, job: JobToPrice }>();
 
+  @ViewChild(TooltipDirective)
+  public tooltip: TooltipDirective;
+
   entityType: PricingMatchEntityTypes;
   entityId: any;
   matchType: string;
 
   handleDataCutClick(job: JobToPrice, jobMatchCut: JobMatchCut) {
     if (!!jobMatchCut.PeerCutId) {
+      this.tooltip.hide();
       this.editCut.emit({jobCut: jobMatchCut, job: job});
     }
   }
