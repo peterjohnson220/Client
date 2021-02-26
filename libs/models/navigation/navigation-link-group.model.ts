9cb3f53d3b821722c6e7d2e010b86bc1b9672b77
vitaly_zisserman <vitaly.zisserman@payfactors.com>
2021-02-26 10:54:26 -0500

    STRUC-703: Company/User link via Site Admin is broken

--- libs/models/navigation/navigation-link-group.model.ts
+++ libs/models/navigation/navigation-link-group.model.ts
@@ -1,51 +1,39 @@
 import { NavigationLink } from './navigation-link.model';
 
 export class NavigationLinkGroup {
-    GroupName: string;
-    Links: NavigationLink[];
-}
-
-export function generateNavigationLinkGroup(): NavigationLinkGroup {
-    return {
-        GroupName: 'MockGroupName',
-        Links: [
-            {
-                Name: 'MockLinkName1',
-                Url: 'http://mock-url-1.url'
-            },
-            {
-                Name: 'MockLinkName2',
-                Url: 'http://mock-url-2.url'
-            }
-        ]
-    };
+  GroupName: string;
+  Links: NavigationLink[];
 }
 
 export function generateNavigationLinkGroupLinks(): NavigationLinkGroup[] {
-    return [{
-        GroupName: 'MockGroupNameA',
-        Links: [
-            {
-                Name: 'MockLinkNameA1',
-                Url: 'http://mock-url-1.url'
-            },
-            {
-                Name: 'MockLinkNameA2',
-                Url: 'http://mock-url-a-2.url'
-            }
-        ]
-    },
+  return [{
+    GroupName: 'MockGroupNameA',
+    Links: [
+      {
+        Name: 'MockLinkNameA1',
+        Url: 'http://mock-url-1.url',
+        NgAppLink: false
+      },
+      {
+        Name: 'MockLinkNameA2',
+        Url: 'http://mock-url-a-2.url',
+        NgAppLink: false
+      }
+    ]
+  },
     {
-        GroupName: 'MockGroupNameB',
-        Links: [
-            {
-                Name: 'MockLinkNameB1',
-                Url: 'http://mock-url-b-1.url'
-            },
-            {
-                Name: 'MockLinkNameB2',
-                Url: 'http://mock-url-b-2.url'
-            }
-        ]
+      GroupName: 'MockGroupNameB',
+      Links: [
+        {
+          Name: 'MockLinkNameB1',
+          Url: 'http://mock-url-b-1.url',
+          NgAppLink: false
+        },
+        {
+          Name: 'MockLinkNameB2',
+          Url: 'http://mock-url-b-2.url',
+          NgAppLink: false
+        }
+      ]
     }];
 }
