d24deaedf79d1d48ac33c94cfa037e3d0786a778
Adam Colson <Adam.Colson@payfactors.com>
2021-03-19 15:13:26 -0400

    TS-798 TRS calc controls use random guids for unique dragula groups

--- libs/features/total-rewards/total-rewards-statement/components/trs-calculation-control/trs-calculation-control.component.ts
+++ libs/features/total-rewards/total-rewards-statement/components/trs-calculation-control/trs-calculation-control.component.ts
@@ -264,7 +264,7 @@ export class TrsCalculationControlComponent implements OnChanges, OnDestroy, OnI
   }
 
   setupDragula(): void {
-    this.dragulaGroupName = 'dragula-group-calc-control-' + this.controlData.Id;
+    this.dragulaGroupName = `dragula-group-calc-control-${this.uuidv4()}`;
     if (this.dragulaService) {
 
       this.dragulaService.createGroup(this.dragulaGroupName, {
@@ -283,5 +283,13 @@ export class TrsCalculationControlComponent implements OnChanges, OnDestroy, OnI
           });
         });
     }
+  }
+
+  // https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+  uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 }
