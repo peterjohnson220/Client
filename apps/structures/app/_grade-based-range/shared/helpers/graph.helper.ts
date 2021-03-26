20cdb48a6d666cc673d048e842a4e1c4ef8a5664
Jake Jesi <jake.jesi@payfactors.com>
2021-03-26 11:00:25 -0400

    Add chart to all jobs view

---
+++ apps/structures/app/_grade-based-range/shared/helpers/graph.helper.ts
@@ -0,0 +1,26 @@
+export class GraphHelper {
+  static getChartHeight(data: any, multiLineHeader: boolean = false): number {
+    const defaultOffset = 56;
+    const multiLineHeaderOffset = 46;
+    const defaultSingleRecordHeight = 120;
+    const rowHeight = 48;
+    const singleDataPointOffset = 10;
+    if (data.length > 1) {
+      return (rowHeight * data.length) + (multiLineHeader ? (multiLineHeaderOffset + defaultOffset) : defaultOffset);
+    } else if (data.length === 1) {
+      return (multiLineHeader ? (multiLineHeaderOffset + defaultSingleRecordHeight + singleDataPointOffset)
+        : defaultSingleRecordHeight + singleDataPointOffset);
+    } else {
+      return (multiLineHeader ? (multiLineHeaderOffset + defaultSingleRecordHeight) : defaultSingleRecordHeight);
+    }
+  }
+  static getCompareChartHeight(dataLength: number, useAlignmentOffset: boolean): number {
+    const defaultOffset = 56;
+    const rowHeight = 60;
+    let scrollOffset = 0;
+    if (useAlignmentOffset) {
+      scrollOffset = 15;
+    }
+    return (rowHeight * dataLength) + (defaultOffset + scrollOffset);
+  }
+}
