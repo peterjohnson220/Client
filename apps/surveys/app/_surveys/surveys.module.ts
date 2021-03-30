35e14089d1ff28fc3526d801ba62fa4155dfcb54
Joshua Brewer <joshua.brewer@payscale.com>
2021-03-30 18:02:04 -0400

    IDEA-1250 New Surveys Page in Client App

---
+++ apps/surveys/app/_surveys/surveys.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StoreModule } from '@ngrx/store';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { PfCommonModule } from 'libs/core';
+import { PfFormsModule } from 'libs/forms';
+import { PfCommonUIModule } from 'libs/ui/common';
+
+import * as fromFaIcons from './fa-icons';
+import { reducers } from './reducers';
+
+import { SurveysPageComponent } from './surveys.page/surveys.page';
+import { SurveysRoutingModule } from './surveys-routing.module';
+
+
+@NgModule({
+  imports: [
+    // Angular
+    CommonModule,
+
+    // Routing
+    SurveysRoutingModule,
+
+    // 3rd party
+    StoreModule.forFeature('surveys_main', reducers),
+    FontAwesomeModule,
+
+    // Payfactors
+    PfCommonModule,
+    PfCommonUIModule,
+    PfFormsModule
+  ],
+  declarations: [
+    // Pages
+    SurveysPageComponent
+  ]
+})
+export class SurveysModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(...fromFaIcons.faIcons);
+  }
+}
---
+++ apps/surveys/app/_surveys/surveys.page/surveys.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pf-surveys-page',
+  templateUrl: './surveys.page.html',
+  styleUrls: ['./surveys.page.scss']
+})
+export class SurveysPageComponent {
+  constructor() {}
+}
+
