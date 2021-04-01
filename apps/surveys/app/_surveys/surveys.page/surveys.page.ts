46a95b023ac1a25074c6d98172dc5d65b8d5963b
B McCulley <bichngoc.mcculley@payfactors.com>
2021-04-01 14:22:53 -0400

    IDEA-1251: Data Grid for Existing Surveys

--- apps/surveys/app/_surveys/surveys.page/surveys.page.ts
+++ apps/surveys/app/_surveys/surveys.page/surveys.page.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
 
+import { SortDescriptor } from '@progress/kendo-data-query';
+
+import { ActionBarConfig, getDefaultActionBarConfig, GridConfig } from 'libs/features/grids/pf-data-grid/models';
+
+import { SurveysPageConfig } from '../models';
+
 @Component({
   selector: 'pf-surveys-page',
   templateUrl: './surveys.page.html',
   styleUrls: ['./surveys.page.scss']
 })
 export class SurveysPageComponent {
-  constructor() {}
+  pageViewId = SurveysPageConfig.SurveysPageViewId;
+  defaultSort: SortDescriptor[] = [{
+    dir: 'asc',
+    field: 'Surveys_Survey_Publisher'
+  }];
+  actionBarConfig: ActionBarConfig;
+  gridConfig: GridConfig;
+
+  constructor() {
+    this.actionBarConfig = {
+      ...getDefaultActionBarConfig(),
+      AllowSaveFilter: false,
+      ShowColumnChooser: true
+    };
+    this.gridConfig = {
+      PersistColumnWidth: false,
+      EnableInfiniteScroll: true,
+      ScrollToTop: true
+    };
+  }
 }
 
