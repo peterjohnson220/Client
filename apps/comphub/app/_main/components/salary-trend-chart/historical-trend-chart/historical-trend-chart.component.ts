54171c66fb8177964a75793bc7f1048133ce7eea
Peter <peter.johnson@payfactors.com>
2021-05-04 12:56:02 -0400

    PEERMIGOS/PEER-875: Adding zoom to trend graph

--- apps/comphub/app/_main/components/salary-trend-chart/historical-trend-chart/historical-trend-chart.component.ts
+++ apps/comphub/app/_main/components/salary-trend-chart/historical-trend-chart/historical-trend-chart.component.ts
@@ -1,23 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
-import { SalaryTrendChartComponent } from '../salary-trend-chart.component';
+import * as Highcharts from 'highcharts/highstock';
+import cloneDeep from 'lodash/cloneDeep';
+
+import { JobSalaryTrend } from '../../../models';
 
 @Component({
   selector: 'pf-historical-trend-chart',
-  templateUrl: '../salary-trend-chart.component.html',
-  styleUrls: ['../salary-trend-chart.component.scss']
+  templateUrl: 'historical-trend-chart.component.html',
+  styleUrls: ['historical-trend-chart.component.scss']
 })
 
-export class HistoricalTrendChartComponent extends SalaryTrendChartComponent {
+export class HistoricalTrendChartComponent implements OnChanges {
+  @Input() salaryTrendData: JobSalaryTrend;
+  @Input() isHourly: boolean;
+  @Input() currencyCode: string;
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chart: Highcharts.Chart;
+  chartOptions: Highcharts.Options = this.getChartOptions();
+
+  absoluteValueOfPercentageChange: number;
+  localSalaryTrendData: JobSalaryTrend;
+  data: any[];
+  img: any;
+
+  constructor(private datePipe: DatePipe, private currencyPipe: CurrencyPipe, private sanitizer: DomSanitizer) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes?.salaryTrendData?.currentValue) {
+      this.absoluteValueOfPercentageChange = this.salaryTrendData.PercentageChange
+        ? Math.abs(this.salaryTrendData.PercentageChange)
+        : 0;
+      this.localSalaryTrendData = cloneDeep(this.salaryTrendData);
+
+      this.refreshChart();
+    }
+
+    if (changes?.isHourly?.currentValue != null && this.localSalaryTrendData != null) {
+      this.refreshChart();
+    }
+  }
 
   refreshChart(): void {
-    this.categories = [];
     this.data = [];
 
     this.localSalaryTrendData.Data.forEach(item => {
-      this.categories.push(this.datePipe.transform(item.EffectiveDate, 'MM/yyyy').toString());
 
-      this.data.push([item.EffectiveDate.toString(), this.isHourly ? item.SalaryHourly : item.SalaryAnnual]);
+      const seconds = Math.floor(new Date(item.EffectiveDate).getTime());
+
+      this.data.push([seconds, this.isHourly ? item.SalaryHourly : item.SalaryAnnual]);
     });
 
     this.chartOptions = this.getChartOptions();
@@ -26,49 +60,73 @@ export class HistoricalTrendChartComponent extends SalaryTrendChartComponent {
   }
 
   getChartOptions(): any {
-
-    let colors = ['#000000'];
-    if (!!this.salaryTrendData && this.salaryTrendData.PercentageChange > 0) {
-      colors = ['#009900'];
-    } else if (!!this.salaryTrendData && this.salaryTrendData.PercentageChange < 0) {
-      colors = ['#a80004'];
-    }
-
     return {
       chart: {
-        type: 'area',
+        type: 'areaspline',
         height: 500,
         width: 1000,
         plotBorderWidth: 1
       },
-      title: {
-        text: 'Salary Trend'
-      },
-      colors: colors,
-      tooltip: {
-        backgroundColor: colors[0],
-        animation: false,
-        style: {
-          color: '#FFFFFF'
+      rangeSelector: {
+        enabled: true,
+        allButtonsEnabled: true,
+        selected: 4,
+        buttons: [{
+          type: 'month',
+          count: 3,
+          text: '3m',
+          title: 'View 3 months'
+        }, {
+          type: 'month',
+          count: 6,
+          text: '6m',
+          title: 'View 6 months'
+        }, {
+          type: 'year',
+          count: 1,
+          text: '1y',
+          title: 'View 1 year'
+        }, {
+          type: 'year',
+          count: 2,
+          text: '2y',
+          title: 'View 2 year'
         },
+        {
+          type: 'year',
+          count: 3,
+          text: '3y',
+          title: 'View 3 year'
+        }]
+      },
+      title: {
+        text: 'Peer Salary Trend'
+      },
+      tooltip: {
+        animation: false,
         formatter: (data) => {
           const point = data.chart.hoverPoint;
-          const transformedDate = point.category;
+          const transformedDate = this.datePipe.transform(new Date(point.category), 'yyyy-MM');
           const transformedSalary = this.currencyPipe.transform(point.y, this.currencyCode, 'symbol-narrow', this.isHourly ? '1.2-2' : '1.0-0');
           return transformedDate + ': ' + transformedSalary;
         }
       },
-      xAxis: {
-        categories: this.categories,
-        labels: {
-          enabled: true
+      plotOptions: {
+        series: {
+          marker: {
+            enabled: false
+          }
         }
+      },
+      xAxis: {
+        type: 'datetime',
+        showLastLabel: 'false'
       },
       yAxis: {
         tickPositioner: function () {
           const positions = [];
-          const increment = (this.dataMax === this.dataMin) ? this.dataMax / 2 : Math.ceil((this.dataMax - this.dataMin) / 2);
-          let tick = Math.floor(this.dataMin - (increment * 2));
+          const increment = (this.dataMax === this.dataMin) ? Math.ceil(this.dataMax / 2000) * 1000 : (Math.ceil((this.dataMax - this.dataMin) / 2000)) * 1000;
+          let tick = Math.abs(Math.floor((this.dataMin - (increment * 2)) / 1000) * 1000);
 
           if (this.dataMax !== null && this.dataMin !== null) {
             for (tick; tick - increment <= this.dataMax; tick += increment) {
@@ -79,24 +137,29 @@ export class HistoricalTrendChartComponent extends SalaryTrendChartComponent {
         },
         title: null,
         labels: {
-          enabled: true
-        }
-      },
-      plotOptions: {
-        area: {
-          marker: {
-            enabled: false,
+          enabled: true,
+          formatter: function() {
+            return '$' + this.value / 1000 + 'K';
           }
-        },
-        series: {
-          pointPlacement: 'on'
         }
       },
       series: [
         {
-          name: 'Peer Trends',
-          lineWidth: 0,
-          data: this.data
+          name: 'Annual Base Pay',
+          type: 'areaspline',
+          data: this.data,
+          fillColor: {
+            linearGradient: {
+              x1: 0,
+              y1: 0,
+              x2: 0,
+              y2: 1
+            },
+            stops: [
+              [0, Highcharts.getOptions().colors[0]],
+              [1, new Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+            ]
+          }
         }
       ],
       credits: {
@@ -106,6 +169,22 @@ export class HistoricalTrendChartComponent extends SalaryTrendChartComponent {
         enabled: false
       }
     };
+  }
+
+  refreshChartImage(): void {
+    if (this.chart) {
+      const svg = this.chart.getSVG(this.chartOptions);
+      const blob = new Blob([svg], {type: 'image/svg+xml'});
+      this.img = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(blob));
+    }
+  }
+
+  rangeChartCallback(chart: Highcharts.Chart = null) {
+    if (chart) {
+      this.chart = chart;
+
+      this.refreshChartImage();
+    }
   }
 }
 
