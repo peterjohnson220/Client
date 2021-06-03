4c4c6310a158f41ddd37e7598eb0f93900f30cac
B McCulley <bichngoc.mcculley@payfactors.com>
2021-06-03 16:33:39 -0400

    IDEA-1363: [Structures] Proposed & Historical Models Tabs

--- apps/structures/app/_structures/structures.page/structures.page.ts
+++ apps/structures/app/_structures/structures.page/structures.page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 import { SortDescriptor } from '@progress/kendo-data-query';
+import { Store } from '@ngrx/store';
 
 import { ActionBarConfig, getDefaultActionBarConfig, GridConfig, PfDataGridFilter } from 'libs/features/grids/pf-data-grid/models';
+import * as fromPfDataGridActions from 'libs/features/grids/pf-data-grid/actions';
+import * as fromPfDataGridReducer from 'libs/features/grids/pf-data-grid/reducers';
 
 import { StructuresPageConfig } from '../models';
 
@@ -22,7 +25,9 @@ export class StructuresPageComponent {
   gridConfig: GridConfig;
   activeTab = 'current-models';
 
-  constructor() {
+  constructor(
+    private pfDataGridStore: Store<fromPfDataGridReducer.State>
+  ) {
     this.actionBarConfig = {
       ...getDefaultActionBarConfig(),
       ShowColumnChooser: true
@@ -32,5 +37,15 @@ export class StructuresPageComponent {
       EnableInfiniteScroll: true,
       ScrollToTop: true
     };
+  }
+
+  handleTabChange(activeTabId: any): void {
+    let inboundFilters = StructuresPageConfig.CurrentModelsInboundFilters;
+    if (activeTabId === StructuresPageConfig.ProposedModelsTabId) {
+      inboundFilters = StructuresPageConfig.ProposedModelsInboundFilters;
+    } else if (activeTabId === StructuresPageConfig.HistoricalModelsTabId) {
+      inboundFilters = StructuresPageConfig.HistoricalModelsInboundFilters;
+    }
+    this.pfDataGridStore.dispatch(new fromPfDataGridActions.UpdateInboundFilters(this.pageViewId, inboundFilters));
   }
 }
