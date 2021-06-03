d178f0d19e55ad715962bfb4f1d5da92284b56d1
B McCulley <bichngoc.mcculley@payfactors.com>
2021-06-03 12:29:09 -0400

    IDEA-1355: [Structures] Tab/Data Grid for Current Models

--- apps/structures/app/_structures/structures.page/structures.page.ts
+++ apps/structures/app/_structures/structures.page/structures.page.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { SortDescriptor } from '@progress/kendo-data-query';
+
+import { ActionBarConfig, getDefaultActionBarConfig, GridConfig, PfDataGridFilter } from 'libs/features/grids/pf-data-grid/models';
+
+import { StructuresPageConfig } from '../models';
+
 @Component({
   selector: 'pf-structures-page',
   templateUrl: './structures.page.html',
   styleUrls: ['./structures.page.scss']
 })
 export class StructuresPageComponent {
+  pageViewId = StructuresPageConfig.StructuresPageViewId;
+  inboundFilters: PfDataGridFilter[] = StructuresPageConfig.CurrentModelsInboundFilters;
+  defaultSort: SortDescriptor[] = [{
+    dir: 'asc',
+    field: 'CompanyStructures_Structure_Name'
+  }];
+  actionBarConfig: ActionBarConfig;
+  gridConfig: GridConfig;
+  activeTab = 'current-models';
 
+  constructor() {
+    this.actionBarConfig = {
+      ...getDefaultActionBarConfig(),
+      ShowColumnChooser: true
+    };
+    this.gridConfig = {
+      PersistColumnWidth: false,
+      EnableInfiniteScroll: true,
+      ScrollToTop: true
+    };
+  }
 }
