b5e0b5fdb6d4ca6e1a70f85582fb124118a98d96
Jake Jesi <jake.jesi@payfactors.com>
2021-06-16 15:45:51 -0400

    Only show Graded/NotGraded filter in GBR add jobs

--- apps/structures/app/shared/data/structures-search-filter-mapping-data.ts
+++ apps/structures/app/shared/data/structures-search-filter-mapping-data.ts
@@ -1,6 +1,54 @@
 import { FilterType, SearchFilterMappingDataObj } from 'libs/features/search/search/models';
 
-export const StructuresSearchFilterMappingDataObj: SearchFilterMappingDataObj = {
+export const JobBasedSearchFilterMappingDataObj: SearchFilterMappingDataObj = {
+  'job_family': {
+    Type: FilterType.Multi,
+    BackingField: 'job_family',
+    DisplayName: 'Job Family',
+    Order: 4,
+    OptionCountDisabled: false,
+    SaveDisabled: false,
+    RefreshOptionsFromServer: true
+  },
+  'job_level': {
+    Type: FilterType.Multi,
+    BackingField: 'job_level',
+    DisplayName: 'Job Level',
+    Order: 6,
+    OptionCountDisabled: false,
+    SaveDisabled: false,
+    RefreshOptionsFromServer: true
+  },
+  'priced': {
+    Type: FilterType.Multi,
+    BackingField: 'is_priced',
+    DisplayName: 'Priced',
+    Order: 8,
+    OptionCountDisabled: false,
+    SaveDisabled: false,
+    RefreshOptionsFromServer: true
+  },
+  'employee_count': {
+    Type: FilterType.Multi,
+    BackingField: 'employee_count',
+    DisplayName: 'Employees',
+    Order: 9,
+    OptionCountDisabled: false,
+    SaveDisabled: false,
+    RefreshOptionsFromServer: true
+  },
+  'priced_jobs': {
+    Type: FilterType.Multi,
+    BackingField: 'priced_jobs',
+    DisplayName: 'Priced',
+    Order: 10,
+    OptionCountDisabled: false,
+    SaveDisabled: false,
+    RefreshOptionsFromServer: true
+  }
+};
+
+export const GradeBasedSearchFilterMappingDataObj: SearchFilterMappingDataObj = {
   'job_family': {
     Type: FilterType.Multi,
     BackingField: 'job_family',
@@ -54,5 +102,5 @@ export const StructuresSearchFilterMappingDataObj: SearchFilterMappingDataObj = 
     OptionCountDisabled: false,
     SaveDisabled: false,
     RefreshOptionsFromServer: true
-  },
+  }
 };
