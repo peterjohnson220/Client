672bc269caff62a06c64dc1f46f947c1bd574921
B McCulley <bichngoc.mcculley@payfactors.com>
2021-06-16 09:58:14 -0400

    IDEA-1368: Flag to Enable New Structures Tile

--- libs/models/url-redirect/url-page.ts
+++ libs/models/url-redirect/url-page.ts
@@ -2,4 +2,5 @@ export class UrlPage {
   static readonly ProjectList = 'project-list';
   static readonly PricingProject = 'pricing-project';
   static readonly Surveys = 'surveys';
+  static readonly Structures = 'structures';
 }
