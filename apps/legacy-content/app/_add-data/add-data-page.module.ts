5fd2a8436cace0292afef4366568db2aff5b4a5d
Edgar Abrahamyan <edgar.Abrahamyan@payfactors.com>
2021-06-23 14:42:22 -0400

    ENG-686
    ENG-686
    
    ENG-686
    
    ENG-686
    
    ENG-686
    
    ENG-686
    
    ENG-686
    
    ENG-686

--- apps/legacy-content/app/_add-data/add-data-page.module.ts
+++ apps/legacy-content/app/_add-data/add-data-page.module.ts
@@ -2,40 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-
 import { PfCommonUIModule } from 'libs/ui/common';
 import { PfFormsModule } from 'libs/forms';
 import { PfSearchModule } from 'libs/features/search/search';
 import { SurveySearchModule } from 'libs/features/surveys/survey-search';
+import {WindowCommunicationService} from 'libs/core/services';
+import {AddDataModule} from 'libs/features/pricings/add-data';
 
 import { AddSurveyDataPageComponent } from './containers';
-import { AddSurveyDataPageEffects } from './effects';
-import { reducers } from './reducers';
-import { AddDataRoutingModule } from './add-data-routing.module';
+import { AddDataPageRoutingModule } from './add-data-page-routing.module';
+
+
 
 @NgModule({
   imports: [
     // Angular
     CommonModule, FormsModule, ReactiveFormsModule,
 
-    // 3rd Party
-    StoreModule.forFeature('project_addData', reducers),
-    EffectsModule.forFeature([AddSurveyDataPageEffects]),
-
     // Routing
-    AddDataRoutingModule,
+    AddDataPageRoutingModule,
 
     // Payfactors
     SurveySearchModule,
     PfCommonUIModule,
     PfFormsModule,
-    PfSearchModule
+    PfSearchModule,
+    AddDataModule
   ],
   declarations: [
     // Pages
     AddSurveyDataPageComponent
-  ]
+  ],
+  providers: [WindowCommunicationService]
 })
-export class AddDataModule { }
+export class AddDataPageModule { }
--- apps/legacy-content/app/_add-data/containers/pages/add-survey-data/add-survey-data.page.ts
+++ apps/legacy-content/app/_add-data/containers/pages/add-survey-data/add-survey-data.page.ts
@@ -1,67 +1,9 @@
-import { Component } from '@angular/core';
-
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { DragulaService } from 'ng2-dragula';
-
-import * as fromSearchReducer from 'libs/features/search/search/reducers';
-import { SearchBaseDirective } from 'libs/features/search/search/containers/search-base';
-import * as fromSearchResultsActions from 'libs/features/search/search/actions/search-results.actions';
-import * as fromSearchFiltersActions from 'libs/features/search/search/actions/search-filters.actions';
-import { SearchFeatureIds } from 'libs/features/search/search/enums/search-feature-ids';
-import { AbstractFeatureFlagService, FeatureFlags } from 'libs/core/services/feature-flags';
-import { DataCutDetails } from 'libs/features/surveys/survey-search/models';
-import * as fromSurveySearchReducer from 'libs/features/surveys/survey-search/reducers';
-import * as fromContextActions from 'libs/features/surveys/survey-search/actions/context.actions';
-import { disableDatacutsDragging } from 'libs/features/surveys/survey-search/helpers';
-import * as fromSurveySearchResultsActions from 'libs/features/surveys/survey-search/actions/survey-search-results.actions';
-import { getSearchFilters, SurveySearchFilterMappingDataObj, SurveySearchUserFilterType } from 'libs/features/surveys/survey-search/data';
-
-import * as fromAddSurveyDataPageActions from '../../../actions/add-survey-data-page.actions';
-import * as fromAddDataReducer from '../../../reducers';
-
+import {Component, Input} from '@angular/core';
 @Component({
   selector: 'pf-add-survey-data-page',
   templateUrl: './add-survey-data.page.html',
   styleUrls: ['./add-survey-data.page.scss']
 })
-export class AddSurveyDataPageComponent extends SearchBaseDirective {
-  selectedCuts$: Observable<DataCutDetails[]>;
-  addingData$: Observable<boolean>;
-  pageShown$: Observable<boolean>;
-  excludeFromParticipation: boolean;
-  featureImplementation = 'component';
-  matchMode: boolean;
-
-  constructor(
-    store: Store<fromSearchReducer.State>,
-    private dragulaService: DragulaService,
-    private featureFlagService: AbstractFeatureFlagService
-  ) {
-    super(store, SurveySearchFilterMappingDataObj, SearchFeatureIds.AddSurveyData, SurveySearchUserFilterType);
-    this.matchMode = this.featureFlagService.enabled(FeatureFlags.SurveySearchLightningMode, false);
-    this.selectedCuts$ = this.store.select(fromSurveySearchReducer.getSelectedDataCuts);
-    this.addingData$ = this.store.select(fromAddDataReducer.getAddingData);
-    this.pageShown$ = this.store.select(fromSearchReducer.getPageShown);
-    this.excludeFromParticipation = false;
-    disableDatacutsDragging(dragulaService);
-  }
-
-  onResetApp() {
-    this.store.dispatch(new fromSearchResultsActions.ClearResults());
-    this.store.dispatch(new fromSurveySearchResultsActions.ClearDataCutSelections());
-    this.excludeFromParticipation = false;
-  }
-
-  onSetContext(payload: any) {
-    this.store.dispatch(new fromSearchFiltersActions.AddFilters(getSearchFilters(this.matchMode)));
-    this.store.dispatch(new fromContextActions.SetProjectSearchContext(payload.SearchContext));
-    this.store.dispatch(new fromContextActions.SetJobContext(payload.JobContext));
-    this.store.dispatch(new fromAddSurveyDataPageActions.ResetAddData());
-  }
-
-  // Event Handling
-  handleAddClicked() {
-    this.store.dispatch(new fromAddSurveyDataPageActions.AddData(this.excludeFromParticipation));
-  }
+export class AddSurveyDataPageComponent {
+  constructor( ) { }
 }
